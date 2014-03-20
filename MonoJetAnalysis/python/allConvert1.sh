a1579bc27844f25bba673da35e530a61b188a9ae
schoefbeck <robert.schoefbeck@cern.ch>
2014-03-20 14:40:34 +0100

    forgot CaloTowerTupelizer

--- MonoJetAnalysis/python/allConvert1.sh
+++ MonoJetAnalysis/python/allConvert1.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
-#python convert.py --samples=data      --chmode=$1&
-python convert.py --samples=ttbar     --chmode=$1 --fromPercentage=$2 --toPercentage=$3&
-#python convert.py --samples=ww        --chmode=$1 &
-#python convert.py --samples=wz        --chmode=$1 &
-#python convert.py --samples=zz        --chmode=$1 &
-#python convert.py --samples=singleTop --chmode=$1 &
-python convert.py --samples=wjetsInc  --chmode=$1 --fromPercentage=$2 --toPercentage=$3&
-python convert.py --samples=dy        --chmode=$1 --fromPercentage=$2 --toPercentage=$3&
-#python convert.py --samples=zinv      --chmode=$1 &
+python convert.py --samples=ttbarPowHeg     --chmode=$1 $2 --fromPercentage=0 --toPercentage=20 &
+python convert.py --samples=ttbarPowHeg     --chmode=$1 $2 --fromPercentage=20 --toPercentage=40 &
+python convert.py --samples=ttbarPowHeg     --chmode=$1 $2 --fromPercentage=40 --toPercentage=60 &
+python convert.py --samples=ttbarPowHeg     --chmode=$1 $2 --fromPercentage=60 --toPercentage=80 &
+python convert.py --samples=ttbarPowHeg     --chmode=$1 $2 --fromPercentage=80 --toPercentage=100 &
+python convert.py --samples=ww              --chmode=$1 $2 &
+python convert.py --samples=wz              --chmode=$1 $2 &
+python convert.py --samples=zz              --chmode=$1 $2 &
+#python convert.py --samples=singleTop       --chmode=$1 $2 &
+#python convert.py --samples=zinv            --chmode=$1 $2 &
+#python convert.py --samples=zinv            --chmode=copy --fromPercentage=0 --toPercentage=20   &
+#python convert.py --samples=zinv            --chmode=copy --fromPercentage=20 --toPercentage=40  &
+#python convert.py --samples=zinv            --chmode=copy --fromPercentage=40 --toPercentage=60  &
+#python convert.py --samples=zinv            --chmode=copy --fromPercentage=60 --toPercentage=80  &
+#python convert.py --samples=zinv            --chmode=copy --fromPercentage=80 --toPercentage=100 &
 
--- MonoJetAnalysis/python/allConvert2.sh
+++ MonoJetAnalysis/python/allConvert2.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
-python convert.py --samples=w1jets  --chmode=$1 --fromPercentage=$2 --toPercentage=$3 &
-python convert.py --samples=w2jets  --chmode=$1 --fromPercentage=$2 --toPercentage=$3 &
-python convert.py --samples=w3jets  --chmode=$1 --fromPercentage=$2 --toPercentage=$3 &
-python convert.py --samples=w4jets  --chmode=$1 --fromPercentage=$2 --toPercentage=$3 &
-#python convert.py --samples=wjets   --chmode=$1  &
-#python convert.py --samples=qcd1    --chmode=$1  &
-#python convert.py --samples=qcd2    --chmode=$1  &
-#python convert.py --samples=qcd3    --chmode=$1  &
+python convert.py --samples=wjetsInc  --chmode=$1 $2 --fromPercentage=0 --toPercentage=50 &
+python convert.py --samples=wjetsInc  --chmode=$1 $2 --fromPercentage=50 --toPercentage=100 &
+python convert.py --samples=dy        --chmode=$1 $2 --fromPercentage=0 --toPercentage=50 &
+python convert.py --samples=dy        --chmode=$1 $2 --fromPercentage=50 --toPercentage=100 &
+python convert.py --samples=w1jets    --chmode=$1 $2 --fromPercentage=0 --toPercentage=50 &
+python convert.py --samples=w1jets    --chmode=$1 $2 --fromPercentage=50 --toPercentage=100 &
+python convert.py --samples=w2jets    --chmode=$1 $2 --fromPercentage=0 --toPercentage=50 &
+python convert.py --samples=w2jets    --chmode=$1 $2 --fromPercentage=50 --toPercentage=100 &
+python convert.py --samples=w3jets    --chmode=$1 $2 --fromPercentage=0 --toPercentage=50 &
+python convert.py --samples=w3jets    --chmode=$1 $2 --fromPercentage=50 --toPercentage=100 &
+python convert.py --samples=w4jets    --chmode=$1 $2 --fromPercentage=0 --toPercentage=50 &
+python convert.py --samples=w4jets    --chmode=$1 $2 --fromPercentage=50 --toPercentage=100 &
+python convert.py --samples=qcd1      --chmode=$1 $2  &
+python convert.py --samples=qcd2      --chmode=$1 $2  &
+python convert.py --samples=qcd3      --chmode=$1 $2  &
 
