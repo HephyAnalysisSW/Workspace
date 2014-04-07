b9f2467a0fb9f227f7b68f18bf050300b4947666
Wolfgang Adam <wolfgang.adam@cern.ch>
2014-04-07 10:54:06 +0200

    added StdPlotsHardMu.py

--- MonoJetAnalysis/plotsWolfgang/StdPlotsHardMu.py
+++ MonoJetAnalysis/plotsWolfgang/StdPlotsHardMu.py
@@ -3,9 +3,10 @@ import math
 import time
 from PlotsBase import *
 from EventHelper import EventHelper
+from LeptonUtilities import *
 from KinematicUtilities import *
 
-class StdPlots(PlotsBase):
+class StdPlotsHardMu(PlotsBase):
 
     def __init__(self,name,preselection=None,elist=None,elistBase="./elists",rebin=None):
         PlotsBase.__init__(self,name,preselection,elist=elist,elistBase=elistBase,rebin=rebin)
@@ -28,29 +29,30 @@ class StdPlots(PlotsBase):
             self.addVariable("met"+sign,100,0.,1000.,'l')
             self.addVariable("ht"+sign,100,0.,1000.,'l')
             self.addVariable("mt"+sign,50,0.,200.,'l')
-            self.addVariable("softMuPt"+sign,100,0.,25.,'u')
+            self.addVariable("softMuPt"+sign,100,0.,250.,'u')
             self.addVariable("softMuEta"+sign,60,0.,3.,'u')
-            self.addVariable("softMuIso"+sign,100,0.,10.,'u')
-            self.addVariable("softMuNPix"+sign,10,0.,10.,'u')
-            self.addVariable("softMuNTk"+sign,20,0.,20.,'u')
-            self.addVariable("softMuDxy"+sign,100,0.,0.1,'u')
-            self.addVariable("softMuDz"+sign,100,0.,0.2,'u')
-            self.addVariable("npv"+sign,100,0.,100.,'u')
             self.addVariable("jetmuDeltaEta"+sign,50,0.,10.,'l')
             self.addVariable("jetmuDeltaR"+sign,50,0.,10.,'l')
             self.addVariable("mlb"+sign,20,0.,100.,'u')
             self.addVariable("drlb"+sign,20,0.,10.,'u')
-            self.addVariable("btag1Pt"+sign,50,0.,250.,'u')
+            self.addVariable("btag1Pt"+sign,50,0.,1000.,'u')
             self.addVariable("btag1Eta"+sign,50,0.,2.5,'u')
             self.addVariablePair("met",40,0.,1000.,"ht",40,0.,1000.,suffix=sign)
+            self.addVariablePair("met",40,0.,1000.,"muPt",40,0.,1000.,suffix=sign)
+            self.addVariablePair("WPt",40,0.,1000.,"met",40,0.,1000.,suffix=sign)
+            self.addVariablePair("WPt",40,0.,1000.,"muPt",40,0.,1000.,suffix=sign)
 
         curdir.cd()
 
     def fill(self,eh,downscale=1):
 
         isrJetPt = eh.get("isrJetPt")
-        softMuPt = eh.get("softIsolatedMuPt")
-        if math.isnan(softMuPt) or math.isnan(isrJetPt):
+        imu = hardestIsolatedMuon(eh,ptmin1=5.,ptmin2=20.,etamax=1.5,reliso=0.5)
+        if imu==None or math.isnan(isrJetPt):
+            return
+
+        softMuPt = eh.get("muPt")[imu]
+        if softMuPt<30:
             return
 
 #        if isrJetPt<350:
@@ -60,7 +62,7 @@ class StdPlots(PlotsBase):
 #        if met<300:
 #            return
 
-        softMuEta = eh.get("softIsolatedMuEta")
+        softMuEta = eh.get("muEta")[imu]
         if abs(softMuEta)>1.5:
             return
 
@@ -70,31 +72,33 @@ class StdPlots(PlotsBase):
         for i in range(njet):
             if jetPts[i]>30:
                 ht += jetPts[i]
-#        if ht<400:
-#            return
-#        if eh.get("softIsolatedMuPdg")<0:
-#            return
-        pdg = eh.get("softIsolatedMuPdg")
+        if ht<300:
+            return
+
+        pdg = eh.get("muPdg")[imu]
         if len(self.charges)==1:
             pdg = 0
 
         jetEtas = eh.get("jetEta")
         jetBtags = eh.get("jetBtag")
         ib = None
+        nb = 0
         for i in range(njet):
 #            if jetPts[i]>30 and jetPts[i]<60 and jetBtags[i]>0.679:
             if jetPts[i]>30 and abs(jetEtas[i])<2.4 and jetBtags[i]>0.679:
                 ib = i
-                break
+                nb += 1
+#                break
         if ib!=None:
             return
+#        if nb<2:
+#            return
         
-        softMuPhi = eh.get("softIsolatedMuPhi")
+        softMuPhi = eh.get("muPhi")[imu]
         metphi = eh.get("type1phiMetphi")
         mt = math.sqrt(2*met*softMuPt*(1-math.cos(metphi-softMuPhi)))
 #        if mt<60 or mt>88:
 #            return
-
 
         self.timers[0].start()
         if self.name!="data":
@@ -103,20 +107,14 @@ class StdPlots(PlotsBase):
             w = 1
         self.timers[0].stop()
         
-        npv = eh.get("ngoodVertices")
-        self.fill1DBySign("npv",pdg,npv,w)
-        softMuIso = eh.get("softIsolatedMuRelIso")*softMuPt
-        self.fill1DBySign("softMuIso",pdg,softMuIso,w)
-        softMuNPix = eh.get("softIsolatedMuPixelHits")
-        self.fill1DBySign("softMuNPix",pdg,softMuNPix,w)
-        softMuNTk = eh.get("softIsolatedMuNumtrackerLayerWithMeasurement")
-        self.fill1DBySign("softMuNTk",pdg,softMuNTk,w)
-        softMuDxy = eh.get("softIsolatedMuDxy")
-        self.fill1DBySign("softMuDxy",pdg,softMuDxy,w)
-        softMuDz = eh.get("softIsolatedMuDz")
-        self.fill1DBySign("softMuDz",pdg,softMuDz,w)
-
         self.fill2DBySign("ht_vs_met",pdg,met,ht,w)
+        self.fill2DBySign("muPt_vs_met",pdg,met,softMuPt,w)
+
+        wPx = softMuPt*math.cos(softMuPhi) + met*math.cos(metphi)
+        wPy = softMuPt*math.sin(softMuPhi) + met*math.sin(metphi)
+        wPt = math.sqrt(wPx**2+wPy**2)
+        self.fill2DBySign("met_vs_WPt",pdg,wPt,met,w)
+        self.fill2DBySign("muPt_vs_WPt",pdg,wPt,softMuPt,w)
 
         self.fill1DBySign("ht",pdg,ht,w)
         self.fill1DBySign("mt",pdg,mt,w)
