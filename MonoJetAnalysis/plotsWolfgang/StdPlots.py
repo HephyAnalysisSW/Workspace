9eeb6407a8872a7b454664c3897f91c6e2cdf62d
Wolfgang Adam <wolfgang.adam@cern.ch>
2014-04-07 10:02:50 +0200

    plots / charge

--- MonoJetAnalysis/plotsWolfgang/StdPlots.py
+++ MonoJetAnalysis/plotsWolfgang/StdPlots.py
@@ -3,29 +3,10 @@ import math
 import time
 from PlotsBase import *
 from EventHelper import EventHelper
+from KinematicUtilities import *
 
-def deltaPhi(phi1,phi2):
-    result = phi2 - phi1
-    if result<-math.pi:
-        result += 2*math.pi
-    if result>math.pi:
-        result -= 2*math.pi
-    return result
-
-def deltaR(phi1,eta1,phi2,eta2):
-    dphi = deltaPhi(phi1,phi2)
-    deta = eta2 - eta1
-    return math.sqrt(dphi*dphi+deta*deta)
-
-        
 class StdPlots(PlotsBase):
 
-#    def addHistogram1D(self,name,nbins,xmin,xmax):
-#        assert name.isalnum()
-#        h1d = ROOT.TH1F(name,name,nbins,xmin,xmax)
-#        self.histogramList.append(h1d)
-#        setattr(self,"h"+name,h1d)
-        
     def __init__(self,name,preselection=None,elist=None,elistBase="./elists",rebin=None):
         PlotsBase.__init__(self,name,preselection,elist=elist,elistBase=elistBase,rebin=rebin)
         self.histogramList = { }
@@ -34,25 +15,34 @@ class StdPlots(PlotsBase):
             ROOT.gROOT.mkdir(name)
         ROOT.gROOT.cd(name)
 
-        self.addVariable("isrJetPt",100,0.,1000.,'l')
-        self.addVariable("isrJetEta",50,0.,5.,'u')
-        self.addVariable("jet2Pt",100,0.,1000.,'u')
-        self.addVariable("jet2Eta",50,0.,5.,'u')
-        self.addVariable("jetPtRatio",50,0.,1.,'u')
-        self.addVariable("njet60",10,-0.5,9.5,'u')
-        self.addVariable("njet",20,-0.5,19.5,'u')
-        self.addVariable("met",100,0.,1000.,'l')
-        self.addVariable("ht",100,0.,1000.,'l')
-        self.addVariable("mt",50,0.,200.,'l')
-        self.addVariable("softMuPt",100,0.,25.,'u')
-        self.addVariable("softMuEta",60,0.,3.,'u')
-        self.addVariable("jetmuDeltaEta",50,0.,10.,'l')
-        self.addVariable("jetmuDeltaR",50,0.,10.,'l')
-        self.addVariable("mlb",20,0.,100.,'u')
-        self.addVariable("drlb",20,0.,10.,'u')
-        self.addVariable("btag1Pt",50,0.,1000.,'u')
-        self.addVariable("btag1Eta",50,0.,2.5,'u')
-        self.addVariablePair("met",40,0.,1000.,"ht",40,0.,1000.)
+        self.charges = [ "Minus", "Plus" ]
+#        self.charges = [ "" ]
+        for sign in self.charges:
+            self.addVariable("isrJetPt"+sign,100,0.,1000.,'l')
+            self.addVariable("isrJetEta"+sign,50,0.,5.,'u')
+            self.addVariable("jet2Pt"+sign,100,0.,1000.,'u')
+            self.addVariable("jet2Eta"+sign,50,0.,5.,'u')
+            self.addVariable("jetPtRatio"+sign,50,0.,1.,'u')
+            self.addVariable("njet60"+sign,10,-0.5,9.5,'u')
+            self.addVariable("njet"+sign,20,-0.5,19.5,'u')
+            self.addVariable("met"+sign,100,0.,1000.,'l')
+            self.addVariable("ht"+sign,100,0.,1000.,'l')
+            self.addVariable("mt"+sign,50,0.,200.,'l')
+            self.addVariable("softMuPt"+sign,100,0.,25.,'u')
+            self.addVariable("softMuEta"+sign,60,0.,3.,'u')
+            self.addVariable("softMuIso"+sign,100,0.,10.,'u')
+            self.addVariable("softMuNPix"+sign,10,0.,10.,'u')
+            self.addVariable("softMuNTk"+sign,20,0.,20.,'u')
+            self.addVariable("softMuDxy"+sign,100,0.,0.1,'u')
+            self.addVariable("softMuDz"+sign,100,0.,0.2,'u')
+            self.addVariable("npv"+sign,100,0.,100.,'u')
+            self.addVariable("jetmuDeltaEta"+sign,50,0.,10.,'l')
+            self.addVariable("jetmuDeltaR"+sign,50,0.,10.,'l')
+            self.addVariable("mlb"+sign,20,0.,100.,'u')
+            self.addVariable("drlb"+sign,20,0.,10.,'u')
+            self.addVariable("btag1Pt"+sign,50,0.,250.,'u')
+            self.addVariable("btag1Eta"+sign,50,0.,2.5,'u')
+            self.addVariablePair("met",40,0.,1000.,"ht",40,0.,1000.,suffix=sign)
 
         curdir.cd()
 
@@ -62,17 +52,16 @@ class StdPlots(PlotsBase):
         softMuPt = eh.get("softIsolatedMuPt")
         if math.isnan(softMuPt) or math.isnan(isrJetPt):
             return
-
 #        if isrJetPt<350:
 #            return
 
         met = eh.get("type1phiMet")
-        if met<300:
-            return
+#        if met<300:
+#            return
 
         softMuEta = eh.get("softIsolatedMuEta")
-#        if abs(softMuEta)>1.5:
-#            return
+        if abs(softMuEta)>1.5:
+            return
 
         njet = int(eh.get("njetCount")+0.5)
         jetPts = eh.get("jetPt")
@@ -82,10 +71,11 @@ class StdPlots(PlotsBase):
                 ht += jetPts[i]
 #        if ht<400:
 #            return
-        if njet>1 or ht>400:
-            return
-        if eh.get("softIsolatedMuPdg")<0:
-            return
+#        if eh.get("softIsolatedMuPdg")<0:
+#            return
+        pdg = eh.get("softIsolatedMuPdg")
+        if len(self.charges)==1:
+            pdg = 0
 
         jetEtas = eh.get("jetEta")
         jetBtags = eh.get("jetBtag")
@@ -112,13 +102,26 @@ class StdPlots(PlotsBase):
             w = 1
         self.timers[0].stop()
         
-        self.hht_vs_met.Fill(met,ht,w)
+        npv = eh.get("ngoodVertices")
+        self.fill1DBySign("npv",pdg,npv,w)
+        softMuIso = eh.get("softIsolatedMuRelIso")*softMuPt
+        self.fill1DBySign("softMuIso",pdg,softMuIso,w)
+        softMuNPix = eh.get("softIsolatedMuPixelHits")
+        self.fill1DBySign("softMuNPix",pdg,softMuNPix,w)
+        softMuNTk = eh.get("softIsolatedMuNumtrackerLayerWithMeasurement")
+        self.fill1DBySign("softMuNTk",pdg,softMuNTk,w)
+        softMuDxy = eh.get("softIsolatedMuDxy")
+        self.fill1DBySign("softMuDxy",pdg,softMuDxy,w)
+        softMuDz = eh.get("softIsolatedMuDz")
+        self.fill1DBySign("softMuDz",pdg,softMuDz,w)
 
-        self.hht.Fill(ht,w)
-        self.hmt.Fill(mt,w)
+        self.fill2DBySign("ht_vs_met",pdg,met,ht,w)
+
+        self.fill1DBySign("ht",pdg,ht,w)
+        self.fill1DBySign("mt",pdg,mt,w)
         
-        self.hisrJetPt.Fill(isrJetPt,w)
-        self.hisrJetEta.Fill(abs(eh.get("isrJetEta")),w)
+        self.fill1DBySign("isrJetPt",pdg,isrJetPt,w)
+        self.fill1DBySign("isrJetEta",pdg,abs(eh.get("isrJetEta")),w)
 
         jetPtRatio = 0.
         jet2Pt = 0.
@@ -129,9 +132,9 @@ class StdPlots(PlotsBase):
             jet2Pt = jetPts[1]
 #            jetEtas = eh.get("jetEta")
             jet2Eta = jetEtas[1]
-        self.hjetPtRatio.Fill(jetPtRatio,w)
-        self.hjet2Pt.Fill(jet2Pt,w)
-        self.hjet2Eta.Fill(jet2Eta,w)
+        self.fill1DBySign("jetPtRatio",pdg,jetPtRatio,w)
+        self.fill1DBySign("jet2Pt",pdg,jet2Pt,w)
+        self.fill1DBySign("jet2Eta",pdg,jet2Eta,w)
 
 
         btag1Pt = 0.
@@ -141,16 +144,16 @@ class StdPlots(PlotsBase):
                 btag1Pt = jetPts[i]
                 btag1Eta = jetEtas[i]
                 break
-        self.hbtag1Pt.Fill(btag1Pt,w)
-        self.hbtag1Eta.Fill(abs(btag1Eta),w)
+        self.fill1DBySign("btag1Pt",pdg,btag1Pt,w)
+        self.fill1DBySign("btag1Eta",pdg,abs(btag1Eta),w)
 
-        self.hnjet60.Fill(eh.get("njet60"),w)
-        self.hnjet.Fill(eh.get("njetCount"),w)
+        self.fill1DBySign("njet60",pdg,eh.get("njet60"),w)
+        self.fill1DBySign("njet",pdg,eh.get("njetCount"),w)
 
-        self.hmet.Fill(met,w)
+        self.fill1DBySign("met",pdg,met,w)
 
-        self.hsoftMuPt.Fill(softMuPt,w)
-        self.hsoftMuEta.Fill(abs(softMuEta),w)
+        self.fill1DBySign("softMuPt",pdg,softMuPt,w)
+        self.fill1DBySign("softMuEta",pdg,abs(softMuEta),w)
            
         jetPhis = eh.get("jetPhi")
         if ib!=None:
@@ -158,11 +161,11 @@ class StdPlots(PlotsBase):
             p4mu.SetPtEtaPhiM(softMuPt,softMuEta,softMuPhi,0.105)
             p4b = ROOT.TLorentzVector()
             p4b.SetPtEtaPhiM(jetPts[ib],jetEtas[ib],jetPhis[ib],5.)
-            self.hmlb.Fill((p4mu+p4b).M(),w)
-            self.hdrlb.Fill(p4mu.DeltaR(p4b),w)
+            self.fill1DBySign("mlb",pdg,(p4mu+p4b).M(),w)
+            self.fill1DBySign("drlb",pdg,p4mu.DeltaR(p4b),w)
 
-        self.hjetmuDeltaEta.Fill(abs(jetEtas[0]-softMuEta),w)
-        self.hjetmuDeltaR.Fill(deltaR(jetPhis[0],jetEtas[0],softMuPhi,softMuEta),w)
+        self.fill1DBySign("jetmuDeltaEta",pdg,abs(jetEtas[0]-softMuEta),w)
+        self.fill1DBySign("jetmuDeltaR",pdg,deltaR(jetPhis[0],jetEtas[0],softMuPhi,softMuEta),w)
 
     def showTimers(self):
         line = ""
