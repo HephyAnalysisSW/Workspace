a284fc5eccc712da1210da0c21363b570c26891e
schoefbeck <robert.schoefbeck@cern.ch>
2014-06-30 19:34:52 +0200

    making TChain an object on the TFile

--- MonoJetAnalysis/python/convert.py
+++ MonoJetAnalysis/python/convert.py
@@ -600,8 +600,10 @@ for isample, sample in enumerate(allSamples):
   if os.path.isfile(ofile) and not overwrite:
     print ofile, "already there! Skipping!!!"
     continue
-  chain_gDir = ROOT.gDirectory.func() 
+  tmp_gDir = ROOT.gDirectory.func() 
+  f = ROOT.TFile(ofile, "recreate")
   t = ROOT.TTree( "Events", "Events", 1 )
+  tmp_gDir.cd()
   t.Branch("event",   ROOT.AddressOf(s,"event"), 'event/l')
   for var in variables:
     t.Branch(var,   ROOT.AddressOf(s,var), var+'/F')
@@ -997,12 +999,12 @@ for isample, sample in enumerate(allSamples):
 
 
           tmpDir = ROOT.gDirectory.func()
-          chain_gDir.cd()
+#          chain_gDir.cd()
 #          print s.type1phiMet
 #          if s.type1phiMet<150:
 #            print "Warning!!"
           t.Fill()
-          tmpDir.cd()
+#          tmpDir.cd()
 #          if s.type1phiMet<150:
 #            print "Warning", s.type1phiMet
 #          else:
@@ -1012,9 +1014,12 @@ for isample, sample in enumerate(allSamples):
       print "Zero entries in", bin, sample["name"]
     del c
   if True or not options.small: #FIXME
-    f = ROOT.TFile(ofile, "recreate")
+    tmpDir = ROOT.gDirectory.func()
+    f.cd()
     t.Write()
     f.Close()
+    tmpDir.cd()
+#    if t:t.IsA().Destructor(t)
     print "Written",ofile
   else:
     print "No saving when small!"
