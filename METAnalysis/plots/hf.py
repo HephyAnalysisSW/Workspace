3719e30a793379b97aafde9ae46f4f93f602b88f
schoefbeck <robert.schoefbeck@cern.ch>
2014-07-08 18:51:08 +0200

    Development of hf.py

--- METAnalysis/plots/hf.py
+++ METAnalysis/plots/hf.py
@@ -1,27 +1,33 @@
 import ROOT
 from math import pi, sin, cos, sqrt
 import numpy as np
+import pickle
 from scipy import optimize
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("--sample", dest="sample", default="dy53X", type="string", action="store", help="samples:Which samples.")
+parser.add_option("--cut", dest="cut", default='candId==6&&candEta>0', type="string", action="store", help="")
+parser.add_option("--postFix", dest="postFix", default='hHFPlus', type="string", action="store", help="")
+
+(options, args) = parser.parse_args()
+
 c = ROOT.TChain('Events')
-#postFix='ttJets'
-#c.Add('/data/schoef/convertedMETTuples_v2/inc/ttJets/histo_ttJets_from0To2.root')
-
-postFix='dy53X'
-c.Add('/data/schoef/convertedMETTuples_v2/inc/dy53X/histo_dy53X_from0To1.root')
-c.Add('/data/schoef/convertedMETTuples_v2/inc/dy53X/histo_dy53X_from1To2.root')
-c.Add('/data/schoef/convertedMETTuples_v2/inc/dy53X/histo_dy53X_from2To3.root')
-c.Add('/data/schoef/convertedMETTuples_v2/inc/dy53X/histo_dy53X_from3To4.root')
-
-#c.Add('/data/schoef/convertedMETTuples_v2/inc/minBiasData/histo_minBiasData_small_from0To50.root')
-#postFix = 'MinBiasSmall'
+if options.sample=='ttJets':
+  c.Add('/data/schoef/convertedMETTuples_v2/inc/ttJets/histo_ttJets_from0To2.root')
+if options.sample=='dy53X':
+  c.Add('/data/schoef/convertedMETTuples_v2/inc/dy53X/histo_dy53X_from0To1.root')
+  c.Add('/data/schoef/convertedMETTuples_v2/inc/dy53X/histo_dy53X_from1To2.root')
+  c.Add('/data/schoef/convertedMETTuples_v2/inc/dy53X/histo_dy53X_from2To3.root')
+  c.Add('/data/schoef/convertedMETTuples_v2/inc/dy53X/histo_dy53X_from3To4.root')
+if options.sample=='minBiasSmall':
+  c.Add('/data/schoef/convertedMETTuples_v2/inc/minBiasData/histo_minBiasData_small_from0To50.root')
+postFix = '_'.join([options.sample, options.postFix])
 
 #c.Add('/data/schoef/convertedMETTuples_v2/inc/DoubleMu-Run2012A-22Jan2013/histo_DoubleMu-Run2012A-22Jan2013_small_from0To50.root')
 ##c.Draw('sqrt(Sum$((candId==6)*candPt*cos(candPhi))**2+Sum$((candId==6)*candPt*sin(candPhi))**2)')
 #c.Draw('atan2(Sum$((candId==6)*candPt*sin(candPhi)),Sum$((candId==6)*candPt*cos(candPhi)))>>(20,-pi,pi)')
 
-
-
-candRequ='candId==6&&candEta>0'
 def getChi2Ndf(x, candRequ, verbose):
 #  aprime=x[0]/1100.
 #  phiHF=x[1]
@@ -29,7 +35,7 @@ def getChi2Ndf(x, candRequ, verbose):
   if abs(x[0])>2. or abs(x[1])>2.: 
     print x
     return 10**9
-##  f = 'atan2(Sum$(('+candRequ+')*candPt*sin(atan2(sin(candPhi)+'+str(aprime)+'*abs(sinh(candEta))*sin('+str(phiHF)+'), cos(candPhi)+'+str(aprime)+'*abs(sinh(candEta))*cos('+str(phiHF)+')))),Sum$(('+candRequ+')*candPt*cos(atan2(sin(candPhi)+'+str(aprime)+'*abs(sinh(candEta))*sin('+str(phiHF)+'), cos(candPhi)+'+str(aprime)+'*abs(sinh(candEta))*cos('+str(phiHF)+')))))'
+#  f = 'atan2(Sum$(('+candRequ+')*candPt*sin(atan2(sin(candPhi)+'+str(aprime)+'*abs(sinh(candEta))*sin('+str(phiHF)+'), cos(candPhi)+'+str(aprime)+'*abs(sinh(candEta))*cos('+str(phiHF)+')))),Sum$(('+candRequ+')*candPt*cos(atan2(sin(candPhi)+'+str(aprime)+'*abs(sinh(candEta))*sin('+str(phiHF)+'), cos(candPhi)+'+str(aprime)+'*abs(sinh(candEta))*cos('+str(phiHF)+')))))'
   f = 'atan2(Sum$(('+candRequ+')*candPt*sin(atan2(sin(candPhi)+abs(sinh(candEta))*('+str(dy)+')/1100., cos(candPhi)+abs(sinh(candEta))*('+str(dx)+')/1100.))),'\
   +'Sum$(('+candRequ+')*candPt*cos(atan2(sin(candPhi)+abs(sinh(candEta))*('+str(dy)+')/1100., cos(candPhi)+abs(sinh(candEta))*('+str(dx)+')/1100.))))'
 
@@ -52,24 +58,30 @@ def getChi2Ndf(x, candRequ, verbose):
 #  fRes=h.Fit('pol0','S')
 #  res = func.GetParameter(1)
 #  if verbose:print 'Chi2',fRes.Chi2(),fRes.Ndf(), 'Target:',res, 'at xHF/phiHF',x 
-  if verbose:print 'Target:',res, 'at dx/dy',x 
+  if verbose:print 'Target:',res, 'at dx/dy',x, 'sample', options.sample 
   del h
   return res 
- 
+
+print "All files:", c.GetListOfFiles().ls() 
+print "nEvents:", c.GetEntries()
 #Optimizing
 bnds = ((-2, 2), (-2, 2))
 x0 = np.array([0.5,0.5])
 #res= optimize.anneal(lambda x:getChi2Ndf(x,candRequ, verbose=True), x0, T0=.0001, learn_rate=1.5)
-res= optimize.fmin(lambda x:getChi2Ndf(x,candRequ, verbose=True), x0)
+res= optimize.fmin(lambda x:getChi2Ndf(x,options.cut, verbose=True), x0)
 #optChi2 = optimize.minimize(lambda x:getChi2Ndf(x,candRequ, verbose=True), x0, bounds=bnds)
 dx, dy = res
+candRequ=options.cut
 c1 = ROOT.TCanvas()
-c.Draw('atan2(Sum$((candId==6)*candPt*sin(candPhi)),Sum$((candId==6)*candPt*cos(candPhi)))>>(20,-pi,pi)')
+c.Draw('atan2(Sum$(('+candRequ+')*candPt*sin(candPhi)),Sum$(('+candRequ+')*candPt*cos(candPhi)))>>hBefore(20,-pi,pi)')
+hBefore=ROOT.gDirectory.Get('hBefore')
 f = 'atan2(Sum$(('+candRequ+')*candPt*sin(atan2(sin(candPhi)+abs(sinh(candEta))*('+str(dy)+')/1100., cos(candPhi)+abs(sinh(candEta))*('+str(dx)+')/1100.))),'\
 +'Sum$(('+candRequ+')*candPt*cos(atan2(sin(candPhi)+abs(sinh(candEta))*('+str(dy)+')/1100., cos(candPhi)+abs(sinh(candEta))*('+str(dx)+')/1100.))))'
-c.Draw(f+'>>h(20,-pi,pi)', '', 'same')
-h=ROOT.gDirectory.Get('h')
-h.SetLineColor(ROOT.kRed)
-h.Draw('same')
-
+c.Draw(f+'>>hAfter(20,-pi,pi)', '', 'same')
+hAfter=ROOT.gDirectory.Get('hAfter')
+hAfter.SetLineColor(ROOT.kRed)
+hAfter.Draw('same')
+result = {'dx':dx, 'dy':dy, 'hBefore':hBefore, 'hAfter':hAfter, 'cut':options.cut, 'nEvents':c.GetEntries(), 'fileList':c.GetListOfFiles()}
 c1.Print('/afs/hephy.at/user/s/schoefbeck/www/pngHF/metPhiHF_'+postFix+'.png')
+c1.Print('/data/schoef/results2014/HFFit//metPhiHF_'+postFix+'.png')
+pickle.dump(result,file( '/data/schoef/results2014/HFFit/metPhiHF_results_'+postFix+'.pkl', 'w'))
