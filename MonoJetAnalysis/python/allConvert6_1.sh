4ccac7c979c681a8e200b12a994fd29e630131e9
schoefbeck <robert.schoefbeck@cern.ch>
2014-07-08 17:44:15 +0200

    added sh files

---
+++ MonoJetAnalysis/python/allConvert6_1.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+python convert.py --chmode=$1 --smsMsqRange=300-325 --samples=sms &
+python convert.py --chmode=$1 --smsMsqRange=325-350 --samples=sms &
+python convert.py --chmode=$1 --smsMsqRange=350-375 --samples=sms &
+python convert.py --chmode=$1 --smsMsqRange=375-400 --samples=sms &
+python convert.py --chmode=$1 --smsMsqRange=400-425 --samples=sms &
+python convert.py --chmode=$1 --smsMsqRange=425-450 --samples=sms &
+python convert.py --chmode=$1 --smsMsqRange=450-475 --samples=sms &
+python convert.py --chmode=$1 --smsMsqRange=475-500 --samples=sms &
+python convert.py --chmode=$1 --smsMsqRange=500-525 --samples=sms &
---
+++ MonoJetAnalysis/python/allConvert6_2.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+python convert.py --chmode=$1 --smsMsqRange=100-125 --samples=sms &
+python convert.py --chmode=$1 --smsMsqRange=125-150 --samples=sms &
+python convert.py --chmode=$1 --smsMsqRange=150-175 --samples=sms &
+python convert.py --chmode=$1 --smsMsqRange=175-200 --samples=sms &
+python convert.py --chmode=$1 --smsMsqRange=200-225 --samples=sms &
+python convert.py --chmode=$1 --smsMsqRange=225-250 --samples=sms &
+python convert.py --chmode=$1 --smsMsqRange=250-275 --samples=sms &
+python convert.py --chmode=$1 --smsMsqRange=275-300 --samples=sms &
