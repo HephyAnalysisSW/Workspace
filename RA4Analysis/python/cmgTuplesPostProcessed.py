597d5b1e3b66d04922802979889f3c113e9f7c7f
schoefbeck <robert.schoefbeck@cern.ch>
2014-10-30 08:42:29 +0100

    cmgTuplesPostProcessed updated

---
+++ RA4Analysis/python/cmgTuplesPostProcessed.py
@@ -0,0 +1,30 @@
+import copy, os, sys
+
+T5Full_1200_1000_800={\
+"name" : "T5Full_1200_1000_800",
+"bins": ["T5Full_1200_1000_800"],
+'dir' : "/data/schoef/cmgTuples/postProcessed_v0/inc/",
+'dbsName':'/T5Full_T5Full-1200-1000-800-Decay-MGMMatch50/schoef-T5Full_T5Full-1200-1000-800-Decay-MGMMatch50-miniAOD-92bfc1aa0ef8c674e0edabb945b19298/USER'
+}
+
+T5Full_1500_800_100={\
+"name" : "T5Full_1500_800_100",
+"bins": ["T5Full_1500_800_100"],
+'dir' : "/data/schoef/cmgTuples/postProcessed_v0/inc/",
+'dbsName':'/T5Full_T5Full-1500-800-100-Decay-MGMMatch50/schoef-T5Full_T5Full-1500-800-100-Decay-MGMMatch50-miniAOD-92bfc1aa0ef8c674e0edabb945b19298/USER'
+}
+
+
+ttJetsCSA1450ns={\
+"name" : "ttJetsCSA1450ns",
+"bins" : ["ttJetsCSA1450ns"],
+'dir' : "/data/schoef/cmgTuples/postProcessed_v0/inc/",
+'dbsName':'/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/Spring14miniaod-PU_S14_POSTLS170_V6-v1/MINIAODSIM'
+}
+
+WJetsHTToLNu={\
+"name" : "WJetsHTToLNu",
+"bins" : ["WJetsToLNu_HT100to200", "WJetsToLNu_HT200to400", "WJetsToLNu_HT400to600", "WJetsToLNu_HT600toInf"],
+'dir' : "/data/schoef/cmgTuples/postProcessed_v0/inc/",
+'dbsName':'/WJetsToLNu_HT-100to200_Tune4C_13TeV-madgraph-tauola/Spring14dr-PU_S14_POSTLS170_V6-v1/AODSIM'
+}
