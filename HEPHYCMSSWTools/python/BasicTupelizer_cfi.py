2205393341f0af548da4b2012d0c2bee7f93a7f2
schoef <robert.schoefbeck@cern.ch>
2014-11-27 10:58:09 +0100

    BasicTupelizer & METAnalysis

---
+++ HEPHYCMSSWTools/python/BasicTupelizer_cfi.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+BasicTupelizer = cms.EDProducer ( "BasicTupelizer",
+    verbose           = cms.untracked.bool(False),
+    vertices = cms.untracked.InputTag("offlinePrimaryVertices"),
+
+    addMSugraOSETInfo = cms.untracked.bool(False),
+    addPDFWeights = cms.untracked.bool(False),
+    metsToMonitor = cms.untracked.vstring([""]),
+    genMetContainer = cms.untracked.InputTag(""),
+    storeGenMet = cms.untracked.bool(False),
+    useForDefaultAlias = cms.untracked.bool(False)
+
+) 
