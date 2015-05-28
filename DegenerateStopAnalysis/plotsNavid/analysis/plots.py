3f0a68ed7184abf3133a550b1ed41487cbbe3f5b
Navid Rad <navid.rad@cern.ch>
2015-05-28 12:51:54 +0200

    bkg and signal smaples for degstop

--- DegenerateStopAnalysis/plotsNavid/analysis/plots.py
+++ DegenerateStopAnalysis/plotsNavid/analysis/plots.py
@@ -4,7 +4,6 @@ from Workspace.HEPHYPythonTools.helpers import getChain, getPlotFromChain, getYi
 #from Workspace.HEPHYPythonTools.helpers import getChunksFromNFS, getChunksFromDPM, getChunks
 #from Workspace.DegenerateStopAnalysis.cmgTuples_v1_Phys14 import *
 
-from Workspace.DegenerateStopAnalysis.cmgTuples_Phys14_v1 import *
 import os
 import math
 
@@ -12,42 +11,19 @@ import math
 #from Workspace.RA4Analysis.cmgTuples_v5_Phys14 import ttJets_fromEOS, 
 
 #from Workspace.DegenerateStopAnalysis.cmgTuplesPostProcessed_v6_Phys14V3 import WJetsToLNu_HT100to200, WJetsToLNu_HT200to400, WJetsToLNu_HT400to600,  WJetsToLNu_HT600toInf, allSignals
-from Workspace.DegenerateStopAnalysis.cmgTuplesPostProcessed_v1_Phys14V5 import *
+from Workspace.DegenerateStopAnalysis.cmgTuples_Phys14_v1 import *
+from Workspace.DegenerateStopAnalysis.cmgTuplesPostProcessed_v3_Phys14V1 import *
 from Workspace.DegenerateStopAnalysis.navidPlotTools import saveCanvas,decorate,decorateAxis
 
 
 targetLumi = 4000. #pb-1
 #lumiWeight = xsec[sample['dbsName']]*target_lumi/float(nTotEvents)
 
-def getChainFromChunks( samples, treeName):
-  c = ROOT.TChain("tree")
-  if type(samples)!=type([]):
-    sampleList=[0]
-    sampleList[0]=samples
-  else:
-    sampleList=samples
-  nTot=0
-  for sample in sampleList:
-    fList, niTot = getChunks(sample,treeName)
-    for f in fList:
-      c.Add(f['file'])
-    #print fList
-    nTot += niTot
-    print c.GetEntries(), nTot, niTot
-  return c, nTot 
-
-
-def getChainFromDir( dir, treeName='tree'):
-  c=ROOT.TChain(treeName)
-  c.Add(dir+"/*.root")
-  return c
 
 
 
-def joinCutStrings(cutStringList):
-  return "(" + " && ".join([ "("+c +")" for c in cutStringList])    +")"
 
-#a, nt=getChunks(T2DegStop_300_270,"treeProducerSusySingleLepton")
+
 
 
 tableDir="/afs/hephy.at/user/n/nrad/www/T2Deg13TeV/analysis/postProcessed_v4/tables/"
@@ -55,46 +31,18 @@ treeName="treeProducerSusySingleLepton"
 ROOT.gStyle.SetOptStat ( 000000)
 
 
-
-
-
-
-#sampleDict={
-#            'TT':  {'tree':getChainFromChunks(ttJets_PU20bx25,treeName) , 'color':31    , 'isSignal':0 , 'isData':0       },
-#            'W':  {'tree':getChainFromChunks([WJetsToLNu_HT100to200_PU20bx25,WJetsToLNu_HT200to400_PU20bx25,WJetsToLNu_HT400to600_PU20bx25,WJetsToLNu_HT600toInf_PU20bx25],treeName) , 'color':41    , 'isSignal':0 , 'isData':0       },
-#            "T2Deg300_270": {'tree':getChainFromChunks(T2DegStop_300_270,treeName), 'color':ROOT.kRed     , 'isSignal':1 , 'isData':0 },
-#            #'TTs': {'tree':getChain(ttJets['soft'],histname='') ,               'color':1    , 'isSignal':0 , 'isData':0       },            
-#         }
-
-
-
 try:
   sampleDict
 except NameError:
   TTSample    = getChain(ttJets['none'],histname='') 
   T2DegSample = getChain(T2DegStop_300_270['none'],histname='')
-  WSample     = getChain(WJetsHTToLNu['none'],histname='')
-
+  #WSample     = getChain(WJetsHTToLNu['none'],histname='')
   sampleDict={
             'TTJets':             {'tree':TTSample    , 'color':31          ,'lineColor':1   , 'isSignal':0 , 'isData':0       },
-            'WJets':              {'tree': WSample    , 'color':424         ,'lineColor':1   , 'isSignal':0 , 'isData':0       },
+            #'WJets':              {'tree': WSample    , 'color':424         ,'lineColor':1   , 'isSignal':0 , 'isData':0       },
             "T2Deg300_270":       {'tree':T2DegSample , 'color':ROOT.kRed  , 'lineColor':1   , 'isSignal':1 , 'isData':0 },
             #'TTs': {'tree':getChain(ttJets['soft'],histname='') ,               'color':1    , 'isSignal':0 , 'isData':0       },            
          }
-
-
-
-  #for s in sampleDict:
-  #  sampleDict[s]['nEvents']=sampleDict[s]['tree'].GetEntries()
-  #  sampleDict[s]['nEvents']=sampleDict[s]['tree'].GetEntries()
-  #  sampleDict[s]['tree'].GetEntry(0)
-  #  sampleDict[s]['xsec']=sampleDict[s]['tree'].GetLeaf('xsec').GetValue(0)
-  #  sampleDict[s]['weight']= ( float(sampleDict[s]['xsec']) * targetLumi ) / float(sampleDict[s]['nTot'])
-  #  print s, sampleDict[s]['weight']
-
-
-
-
 
 
 
@@ -192,6 +140,7 @@ SR1b=SR1+"&&(%s>60)&&(%s<88)"%(mt,mt)
 SR1c=SR1+"&&(%s>88)"%mt
 
 
+
 SR1cutList=[
       ["noCut","(1)"],
       ["LepGood","LepGood_pt"],
@@ -205,6 +154,26 @@ SR1cutList=[
       ["all",SR1],
       ]
 
+
+
+
+
+syncCutList=[
+      #["stCut"," (nLepGood[0]+met_pt > 200)"],
+      ["noCut","(1)"],
+      ["nMuon==1","(nLepGood==1)&&(abs(LepGood_pdgId[0])==13)"],
+      ["dxy0.02 dz0.5","(abs(LepGood_dxy[0])<0.02)&&(abs(LepGood_dz[0])<0.5)"],
+      ["pt30 eta2.1","(LepGood_pt[0]<30.)&&(abs(LepGood_eta[0]<2.1))"],
+      ["charge","LepGood_charge[0]<0."],
+      ["relIso","LepGood_relIso03[0]<0.2"],
+      ["met>200","met_pt>200"],
+      ["nJet25==2","nJet25==2"],
+      ["nBJetLoose25>0","nBJetLoose25>0"],
+      ["jetPt>110","Jet_pt[0]>110."],
+      #["isrJet","isrJet_pt[0]>300."],
+	]
+
+syncCutFlow= makeCutFlowList(syncCutList)
 
 
 cutDict = [
@@ -238,6 +207,8 @@ cutList = [
 
 
 
+
+
 plotDict = {
 
 
@@ -255,33 +226,6 @@ plotDict = {
 
 
 
-getAllAlph = lambda str: ''.join(ch for ch in str if ch not in "Kl13@$%[]{}();'\"")
-
-
-### Move To PlotTools
-
-def getGoodPlotFromChain(c, var, binning,varName='', cutString='(1)', weight='weight', color='', lineWidth='',fillColor='',  binningIsExplicit=False, addOverFlowBin=''): 
-  ret=  getPlotFromChain(c, var, binning, cutString=cutString, weight=weight, binningIsExplicit=binningIsExplicit, addOverFlowBin=addOverFlowBin) 
-  if not varName:
-    varName=getAllAlph(var)
-    print varName
-  ret.SetTitle(varName)
-  ret.SetName(varName)
-  if color:
-    #ret.SetLineColor(color)
-    ret.SetLineColor(color)
-  if lineWidth:
-    ret.SetLineWidth(lineWidth)
-  if fillColor:
-    ret.SetFillColor(fillColor)
-    ret.SetLineColor(ROOT.kBlack)
-  return ret
-
-def getStackFromHists(histList):
-  stk=ROOT.THStack()
-  for h in histList:
-    stk.Add(h)
-  return stk
 
 
 
@@ -290,99 +234,6 @@ def getStackFromHists(histList):
 
 
 
-def matchListToDictKeys(List,Dict):
-  if not List:
-    List=Dict.keys()
-  else:
-    if type(List)==type([]) or  type(List)==type(()):
-      pass
-    else:
-      List=List.rsplit()
-    for l in List:
-      if l not in Dict.keys():
-        print "WARNING: Item \' %s \' will be ignored because it is not found in the dictionary keys:"%(l) , Dict.keys()
-        List.pop(List.index(l))
-  return List
-
-
-
-def getPlots(sampleDict,plotDict,treeList='',varList='',cut=''):
-  """Use: getPlots(sampleDict,plotDict,treeList='',varList='',cut=''):"""
-  treeList  = matchListToDictKeys(treeList,sampleDict)
-  varList   = matchListToDictKeys(varList,plotDict)
-  print treeList
-  print varList
-  for s in treeList:
-    if not sampleDict[s].has_key('plots'):
-      sampleDict[s]['plots']={}
-    for p in varList:
-      lineWidth=plotDict[p]['lineWidth']
-      if plotDict[p]['color'].lower()=="fill" and not sampleDict[s]['isSignal']:
-        #color = ROOT.kBlack
-        color = sampleDict[s]['color']
-        fcolor = sampleDict[s]['color']
-      else:
-        color  = sampleDict[s]['color']
-        fcolor = 0 
-        if sampleDict[s]['isSignal']:
-          lineWidth=2
-      print "Sample:" , s , "Getting Plot: ", p 
-      sampleDict[s]['plots'][p] = getGoodPlotFromChain(sampleDict[s]['tree'] , plotDict[p]['var'], plotDict[p]['bin'],\
-                                                       varName     = p  ,
-                                                       cutString   = "(%s) && (%s)"%(plotDict[p]['presel'],plotDict[p]['cut']),                      
-                                                       color       = color,
-                                                       fillColor   = fcolor,
-                                                       lineWidth   = lineWidth,
-                                                       weight      = 'weight'
-                                                       #weight      = str(sampleDict[s]['weight'])
-                                                       )
-  return
-
-def getBkgSigStacks(sampleDict, plotDict, varList='',treeList=''):
-  """Get stacks for signal and backgrounds. make vars in varlist are available in sampleDict. no stacks for 2d histograms.   """
-  treeList  = matchListToDictKeys(treeList,sampleDict)
-  varList   = matchListToDictKeys(varList,plotDict)
-  #treeList=sampleDict.keys()
-  #varList=plotDict.keys()
-  #sampleDict=sampleDict
-  bkgStackDict={}
-  sigStackDict={}
-  for v in varList:
-    if len(plotDict[v]['bin'])!=6:
-      bkgStackDict[v]=getStackFromHists([ sampleDict[t]['plots'][v] for t in treeList if not sampleDict[t]['isSignal']])
-      sigStackDict[v]=getStackFromHists([ sampleDict[t]['plots'][v] for t in treeList if sampleDict[t]['isSignal']])
-  return (bkgStackDict,sigStackDict)
-
-def drawPlots(sampleDict,plotDict,varList='',treeList='',plotDir=''):
-  treeList  = matchListToDictKeys(treeList,sampleDict)
-  varList   = matchListToDictKeys(varList,plotDict)
-
-  bkgStackDict, sigStackDict =  getBkgSigStacks(sampleDict,plotDict, varList=varList,treeList=treeList)
-  for v in varList:
-    if len(plotDict[v]['bin'])!=6:
-      c1=ROOT.TCanvas("c1","c1")
-      bkgStackDict[v].Draw()
-      bkgStackDict[v].SetMinimum(1)
-      sigStackDict[v].Draw("samenostack")
-      decorateAxis(bkgStackDict[v],xAxisTitle=plotDict[v]['xLabel'],yAxisTitle=plotDict[v]['yLabel'])
-    #plotDict[v]['yAxis']
-      c1.SetLogy(plotDict[v]['yLog'])
-      c1.SetLogx(plotDict[v]['xLog'])
-      c1.Update()
-      saveCanvas(c1,v,plotDir=plotDir)
-      del c1
-    ### 2D plots:
-    elif len(plotDict[v]['bin'])==6:
-      for t in treeList:
-        c1=ROOT.TCanvas("c1","c1")
-        sampleDict[t]['plots'][v].SetTitle(t+"_"+plotDict[v]['title'])
-        sampleDict[t]['plots'][v].Draw("colz")
-        decorateAxis(sampleDict[t]['plots'][v],xAxisTitle=plotDict[v]['xLabel'],yAxisTitle=plotDict[v]['yLabel'])
-        c1.SetLogx(plotDict[v]['xLog'])
-        c1.SetLogy(plotDict[v]['yLog'])
-        c1.SetLogz(plotDict[v]['zLog'])
-        saveCanvas(c1,v+"_"+t,plotDir=plotDir)
-        del c1
 
 
 
@@ -396,148 +247,12 @@ def drawPlots(sampleDict,plotDict,varList='',treeList='',plotDir=''):
 
 
 
-#muFromTau="mediumMuIndex>-1&&muIgpMatch[mediumMuIndex]>-1&&gpTag[muIgpMatch[mediumMuIndex]]==2"
-#preselIvan="(type1phimet_pt>200.&&isrJetPt>110.&&ht>300.)"+"&&"+"(isrJetBTBVetoPassed)"+"&&"+"((nHardElectrons+nHardTaus)==0)"+"&&"+\
-#             "(mediumMuIndex>-1&&(muPt[mediumMuIndex]<20.||nHardMuonsMediumWP==1))"+"&&"+"(njet60<3)"
-#preselRobert="(type1phiMet>200.&&isrJetPt>110.&&ht>300.)"+"&&"+"(isrJetBTBVetoPassed)"+"&&"+"((nHardElectrons+nHardTaus)==0)"+"&&"+\
-#             "(mediumMuIndex>-1&&(muPt[mediumMuIndex]<20.))"+"&&"+"(njet60<3)"
-#preselection=preselRobert
-#SR1="(ht>400.&&type1phiMet>300.)"+"&&"+"(nbtags==0)"+"&&"+"(abs(muEta[mediumMuIndex])<1.5)"+"&&"+"(muPdg[mediumMuIndex]>0)"
-#SR1a=SR1+"&&"+"("+MT + "<60)"
-#SR1b=SR1+"&&"+"("+MT + ">60)&&("+ MT + "< 88)"
-#SR1c=SR1+"&&"+"("+MT + ">88)"
-#SR2="(isrJetPt>325.&&type1phiMet>300.)"+"&&"+"(nHardbtags==0&&nSoftbtags>0)"
-#mtcut = MT+">80"
 
 
+  
+  
 
 
-
-
-def getYieldsFromCutDict(sampleDict,cutDict,treeList=''):
-
-
-  if not treeList:
-    treeList=sampleDict.keys()
-  #varList=plotDict.keys()
-  yieldDict={}
-  for t in treeList:
-    if sampleDict[t].has_key("weight"):
-      print 'using weight value in sampleDict for:  ', t ,  sampleDict[t]['weight']
-      weight = str(sampleDict[t]['weight'])
-    else: weight= "weight"
-    yieldDict[t]={}
-    for cut in cutDict:
-      cutName=cut.keys()[0]
-      cutString=cut[cutName]
-      #cutString = "(%s) && (%s)"%(plotDict[p]['presel'],plotDict[p]['cut'])
-      yieldDict[t][cutName] = getYieldFromChain(sampleDict[t]['tree'],cutString,weight=weight)
-  return yieldDict     
-
-
-addSquareSum = lambda x: math.sqrt(sum(( e**2 for e in x   )))
-
-def getYieldsFromCutList(sampleDict,cutList,treeList='',returnError=True):
-  treeList  = matchListToDictKeys(treeList,sampleDict)
-  print treeList
-
-  if not treeList:
-    treeList=sampleDict.keys()
-  #varList=plotDict.keys()
-  yieldDict={}
-  for t in treeList:
-    ##stupid wjets
-    if t.startswith("W"):
-      pass
-    if sampleDict[t].has_key("weight"):
-      print 'using weight value in sampleDict for:  ', t ,  sampleDict[t]['weight']
-      weight = str(sampleDict[t]['weight'])
-    else: weight= "weight"
-    yieldDict[t]={}
-    for cutName,cutString in cutList:
-      #cutName=cut[0]
-      #cutString=cut[1]
-
-      #cutString=cut[cutName]
-      #cutString = "(%s) && (%s)"%(plotDict[p]['presel'],plotDict[p]['cut'])
-      print cutName
-      if returnError:
-        yieldDict[t][cutName] , yieldDict[t][cutName +"_Err"]= getYieldFromChain(sampleDict[t]['tree'],cutString,weight=weight,returnError=True)
-      else:
-        yieldDict[t][cutName] = getYieldFromChain(sampleDict[t]['tree'],cutString,weight=weight,returnError=False)
-  #########################################################
-  ##   Getting Total Background and FOM for each Signal  ##
-  #########################################################
-  bkgList = [s for s in treeList if not sampleDict[s]['isSignal']]
-  sigList = [s for s in treeList if sampleDict[s]['isSignal']]
-  yieldDict['bkg']={}
-  iBkg=0
-  for cutName, cutString in cutList:
-    yieldDict['bkg'][cutName] = sum([ yieldDict[bkg][cutName] for bkg in bkgList])
-    yieldDict['bkg'][cutName+"_Err"] = addSquareSum([ yieldDict[bkg][cutName+"_Err"] for bkg in bkgList])
-  iSig=0
-  for sig in sigList:
-    fomSig='fom_%s'%sig
-    yieldDict[fomSig]={}
-    for cut in cutList:
-      cutName=cut[0]
-      cutString=cut[1]
-      yieldDict[fomSig][cutName] = yieldDict[sig][cutName]/ math.sqrt(yieldDict['bkg'][cutName])
-      yieldDict[fomSig][cutName+"_Err"] = 0.0
-  #print "added", iBkg+1 , "to bkg"
-  return yieldDict     
-
-def makeTableFromYieldDict(yieldDict,cutList,output="test",saveDir="./"):
-  #orderedYieldDictKeys=["TTJets","WJets","bkg","T2Deg300_270","fom_T2Deg300_270"]
-  orderedYieldDictKeys=["TTJets","WJets","bkg","T2Deg300_270","fom_T2Deg300_270"]
-  #first_col= [ y for y in yieldDict.keys() ]
-  first_col= [ y for y in orderedYieldDictKeys ]
-  first_row= [ cut[0] for cut in cutList ]
-
-  if not output.endswith(".tex"): output += ".tex"
-  f2=open( output,'wb')
-  f2.write( '\documentclass{article} \n\usepackage[english]{babel}\n \usepackage[margin=0.1in]{geometry}  \n\usepackage[T1]{fontenc}\n\\begin{document}\
-              \n\\begin{center}\n\\begin{tabular}')
-  f2.write(' { |c | '+ '|'.join( 'l' for icol in first_col ) +'| } \n' )
-  f2.write('\\hline \n')
-  f2.write( '      ' +' & '+ '&'.join(icol.replace("_","\_") for icol in first_col) + ' \\\ \hline \n' )
-  for row in first_row:
-    
-    f2.write( '%s& '%row.replace("_","\_") + ' & '.join([ format(yieldDict[t][row],"0.2f") +" $\pm$ " + format(yieldDict[t][row+"_Err" ],"0.2f") for t in orderedYieldDictKeys     ]))
-    f2.write('\\\ \hline \n')
-  f2.write( '\end{tabular}\n\end{center}\n\end{document}')
-  f2.close()
-  print 'tex file written'
-  if saveDir:
-    os.system("pdflatex -output-directory=%s %s"%(saveDir,outName))
-  return
-
-
-
-
-#plots = [
-#          ['sqrt(2*leptonPt*met*(1-cos(metPhi-leptonPhi)))', [20,0,800], 'mt'],\
-#          ['acos((leptonPt + met*cos(leptonPhi - metPhi))/sqrt(leptonPt**2 + met**2+2*met*leptonPt*cos(leptonPhi-metPhi)))', [16,0,3.2], 'd
-#phi']
-#    ]
-#
-#
-#
-#for var, binning, fname in plots:
-#  c1 = ROOT.TCanvas() 
-#  hPresel = getPlotFromChain(c, var, binning, presel, 'weight')
-#  hPresel.SetLineColor(ROOT.kBlack)
-#  hPresel.SetTitle("")
-#  hPresel.GetYaxis().SetRangeUser(0.1, 1.5*hPresel.GetMaximum())
-#  hPresel.Draw()
-#
-#  for sig in signals:
-#    sig['hPresel'] = getPlotFromChain(sig['c'], var, binning, presel, 'weight')
-#    sig['hPresel'].SetLineColor(sig['color'])
-#    sig['hPresel'].SetLineWidth(2)
-#    sig['hPresel'].SetTitle("")
-#
-#
 
 #
 #
---
+++ DegenerateStopAnalysis/python/cmgTuplesPostProcessed_v3_Phys14V1.py
@@ -0,0 +1,86 @@
+import copy, os, sys
+
+dir1="/afs/hephy.at/work/n/nrad/data/postProcessed_v3_Phys14V1/"
+
+
+def makeSample(sample):
+  n = copy.deepcopy(sample)
+  n['dir']=n['dir']+'/none/'
+  h = copy.deepcopy(sample)
+  h['dir']=h['dir']+'/hard/'
+  s = copy.deepcopy(sample)
+  s['dir']=s['dir']+'/soft/'
+  return {'hard':h, 'soft':s, 'none':n}
+
+ttJets=makeSample({\
+"name" : "ttJets",
+"bins" : ["TTJets"],
+'dir' : dir1,
+})
+WJetsHTToLNu=makeSample({\
+"name" : "WJetsHTToLNu",
+"bins" : ["WJetsToLNu_HT100to200", "WJetsToLNu_HT200to400", "WJetsToLNu_HT400to600", "WJetsToLNu_HT600toInf"],
+'dir' : dir1,
+})
+TTVH=makeSample({\
+"name" : "TTVH",
+"bins" : ["TTH", "TTWJets", "TTZJets"],
+'dir' : dir1,
+})
+singleTop=makeSample({\
+"name" : "singleTop",
+"bins" : ["TBarToLeptons_sch", "TBarToLeptons_tch", "TBar_tWch", "TToLeptons_sch", "TToLeptons_tch", "T_tWch"],
+'dir' : dir1,
+})
+DY=makeSample({\
+"name" : "DY",
+"bins" : ["DYJetsToLL_M50_HT100to200", "DYJetsToLL_M50_HT200to400", "DYJetsToLL_M50_HT400to600", "DYJetsToLL_M50_HT600toInf"],
+'dir' : dir1,
+})
+QCD=makeSample({\
+"name" : "QCD",
+"bins" : ["QCD_HT_100To250","QCD_HT_250To500", "QCD_HT_500To1000", "QCD_HT_1000ToInf"],
+'dir' : dir1,
+})
+
+
+#T1qqqq_1400_325_300={\
+#"name" : "T1qqqq_1400_325_300",
+#"bins": ["T1qqqq_1400_325_300"],
+#'dir' : "/data/easilar/cmgTuples/postProcessed_v0/singleLepton/",
+#}
+#
+#T5Full_1200_1000_800={\
+#"name" : "T5Full_1200_1000_800",
+#"bins": ["T5Full_1200_1000_800"],
+#'dir' : "/data/easilar/cmgTuples/postProcessed_v0/singleLepton/",
+#}
+#
+#T5Full_1500_800_100={\
+#"name" : "T5Full_1500_800_100",
+#"bins": ["T5Full_1500_800_100"],
+#'dir' : "/data/easilar/cmgTuples/postProcessed_v0/singleLepton/",
+#}
+
+
+allSignalStrings=[\
+"T2DegStop_300_270",
+]
+
+
+def getSignalSample(signal):
+  if signal in allSignalStrings:
+    return {
+      "name" : signal,
+#      "chunkString": signal,
+      'dir' : dir1,
+      'bins':[signal]}
+  else:
+    print "Signal",signal,"unknown. Available: ",", ".join(allSignalStrings)
+
+allSignals=[]
+for s in allSignalStrings:
+  sm = makeSample(getSignalSample(s))
+  exec(s+"=sm")
+  exec("allSignals.append(s)")
+  
