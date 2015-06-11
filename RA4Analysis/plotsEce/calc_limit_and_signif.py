c5abb0ffbdb0b8a5a76aeed1053305ddf466b4bc
Ece Asilar <ece.asilar@cern.ch>
2015-06-11 14:01:44 +0200

    limit siliding dPhi

--- RA4Analysis/plotsEce/calc_limit_and_signif.py
+++ RA4Analysis/plotsEce/calc_limit_and_signif.py
@@ -5,13 +5,18 @@ import ROOT
 import pickle
 import array
 import numpy as n
-#ROOT.gROOT.LoadMacro("/afs/hephy.at/scratch/e/easilar/CMSSW_7_2_3/src/Workspace/HEPHYPythonTools/scripts/root/tdrstyle.C")
-#ROOT.setTDRStyle()
+ROOT.gROOT.LoadMacro("/afs/hephy.at/work/e/easilar/Working_directory/CMSSW_7_1_5/src/Workspace/HEPHYPythonTools/scripts/root/tdrstyle.C")
+ROOT.setTDRStyle()
 
-path = "/afs/hephy.at/user/e/easilar/www/PHYS14v3/limit_results/singleLeptonic/combined_bin/"
+path = "/afs/hephy.at/user/e/easilar/www/PHYS14v3/limit_results/singleLeptonic/combined_bin_tests/"
 #path = "/afs/hephy.at/user/e/easilar/www/PHYS14v3/limit_results/singleMuonic/combined_bin_JJ/"
 if not os.path.exists(path):
   os.makedirs(path)
+
+path_table = "/afs/hephy.at/user/e/easilar/www/PHYS14v3/limit_results/singleLeptonic/combined_bin_tests/tables/"
+if not os.path.exists(path_table):
+  os.makedirs(path_table)
+
 
 text_path = "text_files"
 if not os.path.exists(text_path):
@@ -22,31 +27,24 @@ if not os.path.exists(text_path):
 
 
 options = ['signif' , 'limit']
-option = options[0]
+option = options[1]
 
-lumi_bins = [1,2,3,4,5,6,7,8,9,10]
-lumi_origin = 4
+#lumi_bins = [1,2,3,4,5,6,7,8,9,10]
+lumi_bins = [3,10]
+lumi_origin = 3
 
 #bin_yields1 , bin_yields2 = pickle.load(file('/data/easilar/PHYS14v3/SRfinder/singleLeptonic_SRfinder_pkl'))
 #bin_yields1 , bin_yields2 = pickle.load(file('/data/dhandl/results2015/SRfinder/singleMuonic_SRfinder_Phys14V3_adddPhiJJcut_pkl'))
 #bin_yields1 , bin_yields2 = pickle.load(file('/data/dhandl/results2015/SRfinder/singleMuonic_SRfinder_Phys14V3_pkl'))
-bin_yields1 , bin_yields2 , bin_yields3 = pickle.load(file('/data/easilar/results2015/SRfinder/singleLeptonic_SRfinder_Phys14V3_pkl'))
+#bin_yields1 , bin_yields2 , bin_yields3 = pickle.load(file('/data/easilar/results2015/SRfinder/singleLeptonic_SRfinder_Phys14V3_pkl'))
+#bin_yields2 = pickle.load(file('/data/dspitzbart/yields3fb_pkl'))
+#bin_yields2 = pickle.load(file('/data/dspitzbart/lumi3.0yields_pkl'))
+bin_yields2 = pickle.load(file('/data/dspitzbart/lumi3.0yields_pkl_newOptMoreBins'))
 
 search_bins = [\
-             {'HT': (1250 , -1)   , 'ST': (450,-1) , 'nJet': (8,-1)},\
-             {'HT': (1250 , -1)   , 'ST': (450,-1) , 'nJet': (6,7)},\
-             {'HT': (1250 , -1)   , 'ST': (450,-1) , 'nJet': (5,5)},\
-             ###{'HT': (1250 , -1)   , 'ST': (350,450) , 'nJet': (8,-1)},\
-             {'HT': (1250 , -1) , 'ST': (450,-1) , 'nJet': (5,5)},\
-             #{'HT': (1000 , 1250) , 'ST': (450,-1) , 'nJet': (5,5)},\
-             {'HT': (1000 , 1250) , 'ST': (450,-1) , 'nJet': (6,7)},\
-             ##{'HT': (1000 , 1250) , 'ST': (450,-1) , 'nJet': (8,-1)},\
-            #{'HT': (750 , 1000) , 'ST': (450,-1) , 'nJet': (8,-1)},\
-            #{'HT': (750 , 1000) , 'ST': (250,350) , 'nJet': (8,-1)},\
-            #{'HT': (750 , 1000) , 'ST': (450,-1) , 'nJet': (6,7)},\
-            #{'HT': (500 , 750) , 'ST': (450,-1) , 'nJet': (6,7)},\
-            #{'HT': (500 , 750) , 'ST': (350,450) , 'nJet': (6,7)},\
-             ##{'HT': (750 , 1000) , 'ST': (350,450) , 'nJet': (8,-1)},\
+              {'HT': (500  ,750)   , 'ST': (450,-1) , 'nJet': (6,7)},\
+              {'HT': (750 , 1250)  , 'ST': (450,-1) , 'nJet': (6,7)},\
+              {'HT': (1250 , -1)  , 'ST': (450,-1) , 'nJet': (6,7)},\
             ]
 
 found_bin = [bin_yield for bin_yield in bin_yields2 for search_bin in search_bins if (bin_yield['HT'] == search_bin['HT'] and bin_yield['ST'] == search_bin['ST'] and bin_yield['nJet'] == search_bin['nJet'])]
@@ -58,7 +56,7 @@ signals = [
           {'name': 'S1200' ,'label': 'T5q^{4} 1.2_1.0_0.8'}, \
           {'name': 'S1000' ,'label': 'T5q^{4} 1.0_0.8_0.7'}, \
          ]
-signal = signals[0]
+signal = signals[2]
 print signal
 tot_b_Y = sum(bin['B'] for bin in found_bin)
 tot_s_Y = sum(bin[signal['name']] for bin in found_bin)
@@ -67,6 +65,7 @@ sigma = 0
 x_s = n.zeros(11, dtype=float)
 y_s = n.zeros(11, dtype=float)
 for lum in lumi_bins:
+  print "lum:" , lum
   print "lumi:" , lum*1000
   c = cardFileWriter()
   c.defWidth=12
@@ -107,11 +106,13 @@ for lum in lumi_bins:
     limit = c.calcSignif()
     limit_sig = limit['-1.000']
     print "significance :" , limit_sig
-    if lum == 4 : sigma = limit_sig
+    if lum == lumi_origin : sigma = limit_sig
     y_s[int(lum)] = limit_sig
     x_s[int(lum)] = int(lum)
 
 
+
+###For ploting ###
 can = ROOT.TCanvas("can","can",600,600)
 can.cd()
 latex = ROOT.TLatex()
@@ -134,7 +135,7 @@ leg.AddEntry(g_limit,signal['label'] ,"l")
 #leg.AddEntry(g_limit, "T5q^{4} 1.5_0.8_0.1" ,"l")
 leg.Draw()
 latex.DrawLatex(0.16,0.96,"CMS Simulation")
-latex.DrawLatex(0.71,0.96,"L=4 fb^{-1} (13 TeV)")
+latex.DrawLatex(0.71,0.96,"L="+str(lumi_origin)+" fb^{-1} (13 TeV)")
 latex.DrawLatex(0.55,0.31,"bkg yield:"+str(round(tot_b_Y,2)))
 latex.DrawLatex(0.55,0.35,"signal yield:"+str(round(tot_s_Y,2)))
 latex.DrawLatex(0.55,0.39,"#sigma:"+str(round(sigma,2)))
