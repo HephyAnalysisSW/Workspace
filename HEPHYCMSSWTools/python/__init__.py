1e4b9db742e119dc0c838cac59afb9e313824b9a
schoefbeck <robert.schoefbeck@cern.ch>
2015-07-20 09:41:08 +0200

    refactored MetAnalysis

--- HEPHYCMSSWTools/python/__init__.py
+++ HEPHYCMSSWTools/python/__init__.py
@@ -1,3 +1,3 @@
 #Automatically created by SCRAM
 import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/Workspace/HEPHYCMSSWTools/',1)[0])+'/cfipython/slc6_amd64_gcc481/Workspace/HEPHYCMSSWTools')
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/Workspace/HEPHYCMSSWTools/',1)[0])+'/cfipython/slc6_amd64_gcc491/Workspace/HEPHYCMSSWTools')
--- MetAnalysis/python/allConvert_ee.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-python convert.py Zee data_ee &
-python convert.py Zee ttbar_ee &
-python convert.py Zee wjets_ee &
-python convert.py Zee drellYan_ee &
-python convert.py Zee singleTop_ee &
-python convert.py Zee diboson_ee &
-
