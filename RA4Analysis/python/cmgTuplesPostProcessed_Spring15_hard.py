085124625ee06f521a0aadf05e0c150b1e415636
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2015-08-13 16:12:12 +0200

    re-postprocessed Wjets

--- RA4Analysis/python/cmgTuplesPostProcessed_Spring15_hard.py
+++ RA4Analysis/python/cmgTuplesPostProcessed_Spring15_hard.py
@@ -27,8 +27,8 @@ ttJets=makeSample({\
 
 WJetsHTToLNu=makeSample({\
 "name" : "WJetsHTToLNu",
-"bins" : ["WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8", "WJetsToLNu_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8", "WJetsToLNu_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8", "WJetsToLNu_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"],
-'dir' : "/data/easilar/cmgTuples/postProcessed_Spring15_AllFlags/",
+"bins" : ["WJetsToLNu_HT-100To200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1_MC25ns", "WJetsToLNu_HT-200To400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1_MC25ns", "WJetsToLNu_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3_MC25ns", "WJetsToLNu_HT-600ToInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1_MC25ns"],
+'dir' : "/data/dspitzbart/cmgTuples/postProcessed_Spring15_AllFlags2/",
 })
 
 #WJetsHTToLNu=makeSample({\
