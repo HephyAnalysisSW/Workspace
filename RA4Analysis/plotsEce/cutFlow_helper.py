020b452ccffc6abf45a505e414070c8e4a32a314
Ece Asilar <ece.asilar@cern.ch>
2015-08-17 15:04:17 +0200

    cut Flow , samples , ploting

--- RA4Analysis/plotsEce/cutFlow_helper.py
+++ RA4Analysis/plotsEce/cutFlow_helper.py
@@ -1,9 +1,11 @@
 minPt_Lep = 25  ##hard leptons
 minPt_Lep_loose = 10
-minPt_e_loose = 7
-minPt_mu_loose = 5
+minPt_e_loose = 10
+minPt_e_veto = 10
+minPt_mu_veto = 10
+minPt_mu_loose = 10
 maxEta_e = 2.5
-maxEta_mu = 2.4
+maxEta_mu = 2.5
 minPt_Jet = 30
 maxEta_Jet = 2.4
 ID_mu=1
@@ -12,81 +14,73 @@ minRelIso_lep_loose=0.4
 minRelIso_e=0.1
 minRelIso_mu=0.2
 max_sip3d = 4.0
-min_njets = 4
-min_st = 200
+min_njets = 2
+min_st = 250
 min_ht = 500
 min_DPhi = 1
 nbjet = 0
-btag_var = 0.814
+btag_var = 0.890
 
+
+######muon  selection####
+
+mu_veto       = "(Sum$(abs(LepGood_pdgId)==13&&LepGood_pt>="+str(minPt_mu_veto)+"))" #+"&&LepGood_miniRelIso<"+str(minRelIso_lep_loose)+"&&LepGood_mediumMuonId=="+str(ID_mu)+"))"
+mu_tight      = "(Sum$(abs(LepGood_pdgId)==13&&LepGood_pt>="+str(minPt_Lep)+"&&abs(LepGood_eta)<"+str(maxEta_mu)+"&&LepGood_miniRelIso<"+str(minRelIso_mu)+"&&LepGood_mediumMuonId=="+str(ID_mu)+"&&LepGood_sip3d<"+str(max_sip3d)+"))"
+
+
+
+######electron selection#####
 ele_MVAID_cuts_loose={'eta08':0.35 , 'eta104':0.20,'eta204': -0.52}
 ele_MVAID_cuts_tight={'eta08':0.73 , 'eta104':0.57,'eta204':  0.05}
 
 ele_MVAID_cutstr_loose= "((abs(LepGood_eta)<0.8&&LepGood_mvaIdPhys14>"+ str(ele_MVAID_cuts_loose['eta08'])+")"\
-                       +"||((abs(LepGood_eta)>0.8&&abs(LepGood_eta)<1.44)&&LepGood_mvaIdPhys14>"+ str(ele_MVAID_cuts_loose['eta104'])+")"\
-                       +"||((abs(LepGood_eta)>1.57)&&LepGood_mvaIdPhys14>"+str(ele_MVAID_cuts_loose['eta204'])+"))"
+                       +"||((abs(LepGood_eta)>=0.8&&abs(LepGood_eta)<1.44)&&LepGood_mvaIdPhys14>"+ str(ele_MVAID_cuts_loose['eta104'])+")"\
+                       +"||((abs(LepGood_eta)>=1.57)&&LepGood_mvaIdPhys14>"+str(ele_MVAID_cuts_loose['eta204'])+"))"
 
 ele_MVAID_cutstr_tight= "((abs(LepGood_eta)<0.8&&LepGood_mvaIdPhys14>"+ str(ele_MVAID_cuts_tight['eta08'])+")"\
-                       +"||((abs(LepGood_eta)>0.8&&abs(LepGood_eta)<1.44)&&LepGood_mvaIdPhys14>"+ str(ele_MVAID_cuts_tight['eta104'])+")"\
-                       +"||((abs(LepGood_eta)>1.57)&&LepGood_mvaIdPhys14>"+str(ele_MVAID_cuts_tight['eta204'])+"))"
+                       +"||((abs(LepGood_eta)>=0.8&&abs(LepGood_eta)<1.44)&&LepGood_mvaIdPhys14>"+ str(ele_MVAID_cuts_tight['eta104'])+")"\
+                       +"||((abs(LepGood_eta)>=1.57)&&LepGood_mvaIdPhys14>"+str(ele_MVAID_cuts_tight['eta204'])+"))"
 
 ele_Eta_acc_cut_str = "((abs(LepGood_eta)<1.44||abs(LepGood_eta)>1.57)&&abs(LepGood_eta)<"+str(maxEta_e)+")"
 
-mu_veto       = "(Sum$(abs(LepGood_pdgId)==13&&LepGood_pt>="+str(minPt_mu_loose)+"))" #+"&&LepGood_miniRelIso<"+str(minRelIso_lep_loose)+"&&LepGood_mediumMuonId=="+str(ID_mu)+"))"
-mu_loose      = "(Sum$(abs(LepGood_pdgId)==13&&LepGood_pt>="+str(minPt_mu_loose)+"&&abs(LepGood_eta)<"+str(maxEta_mu)+"&&LepGood_miniRelIso<"+str(minRelIso_lep_loose)+"&&LepGood_mediumMuonId=="+str(ID_mu)+"&&LepGood_sip3d<"+str(max_sip3d)+"))"
-mu_loose_hard = "(Sum$(abs(LepGood_pdgId)==13&&LepGood_pt>="+str(minPt_Lep)+"&&abs(LepGood_eta)<"+str(maxEta_mu)+"&&LepGood_miniRelIso<"+str(minRelIso_lep_loose)+"&&LepGood_mediumMuonId=="+str(ID_mu)+"&&LepGood_sip3d<"+str(max_sip3d)+"))"
-mu_tight      = "(Sum$(abs(LepGood_pdgId)==13&&LepGood_pt>="+str(minPt_Lep)+"&&abs(LepGood_eta)<"+str(maxEta_mu)+"&&LepGood_miniRelIso<"+str(minRelIso_mu)+"&&LepGood_tightId=="+str(ID_mu)+"&&LepGood_sip3d<"+str(max_sip3d)+"))"
 
-mu_soft_loose_10Pt = "(Sum$(abs(LepGood_pdgId)==13&&LepGood_pt>=10&&LepGood_pt<25"+"&&"+"abs(LepGood_eta)<"+str(maxEta_mu)+"&&LepGood_miniRelIso<"+str(minRelIso_lep_loose)+"&&LepGood_mediumMuonId=="+str(ID_mu)+"&&LepGood_sip3d<"+str(max_sip3d)+"))"
+e_veto        = "(Sum$(abs(LepGood_pdgId)==11&&LepGood_pt>="+str(minPt_e_veto)+"))" #+"&&"+ele_Eta_acc_cut_str+"&&LepGood_miniRelIso<"+str(minRelIso_lep_loose)+"&&"+ele_MVAID_cutstr_loose+"))" 
+e_tight       = "(Sum$(abs(LepGood_pdgId)==11&&LepGood_pt>="+str(minPt_Lep)+"&&abs(LepGood_eta)<2.4&&LepGood_miniRelIso<"+str(minRelIso_e)+"&&"+ele_MVAID_cutstr_tight+"&&"+"LepGood_lostHits==0"+"&&"+"LepGood_convVeto"+"&&"+"LepGood_sip3d<"+str(max_sip3d)+"))" 
 
-e_veto        = "(Sum$(abs(LepGood_pdgId)==11&&LepGood_pt>="+str(minPt_e_loose)+"))" #+"&&"+ele_Eta_acc_cut_str+"&&LepGood_miniRelIso<"+str(minRelIso_lep_loose)+"&&"+ele_MVAID_cutstr_loose+"))" 
-e_loose       = "(Sum$(abs(LepGood_pdgId)==11&&LepGood_pt>="+str(minPt_e_loose)+"&&"+ele_Eta_acc_cut_str+"&&LepGood_miniRelIso<"+str(minRelIso_lep_loose)+"&&"+ele_MVAID_cutstr_loose+"&&"+"LepGood_lostHits<=1"+"&&"+"LepGood_convVeto"+"&&"+"LepGood_sip3d<"+str(max_sip3d)+"))" 
-e_loose_hard  = "(Sum$(abs(LepGood_pdgId)==11&&LepGood_pt>="+str(minPt_Lep)+"&&"+ele_Eta_acc_cut_str+"&&LepGood_miniRelIso<"+str(minRelIso_lep_loose)+"&&"+ele_MVAID_cutstr_loose+"&&"+"LepGood_lostHits<=1"+"&&"+"LepGood_convVeto"+"&&"+"LepGood_sip3d<"+str(max_sip3d)+"))" 
-e_tight       = "(Sum$(abs(LepGood_pdgId)==11&&LepGood_pt>="+str(minPt_Lep)+"&&"+ele_Eta_acc_cut_str+"&&LepGood_miniRelIso<"+str(minRelIso_e)+"&&"+ele_MVAID_cutstr_tight+"&&"+"LepGood_lostHits<=1"+"&&"+"LepGood_convVeto"+"&&"+"LepGood_sip3d<"+str(max_sip3d)+"&&LepGood_tightId>="+str(minID_e)+"))" 
-
-e_soft_loose_10Pt = "(Sum$(abs(LepGood_pdgId)==11&&LepGood_pt>=10&&LepGood_pt<25"+"&&"+ele_Eta_acc_cut_str+"&&LepGood_miniRelIso<"+str(minRelIso_lep_loose)+"&&"+ele_MVAID_cutstr_loose+"&&"+"LepGood_lostHits<=1"+"&&"+"LepGood_convVeto"+"&&"+"LepGood_sip3d<"+str(max_sip3d)+"))" 
-
-
-nLooseLeptons     = "("+mu_loose+"+"+e_loose+")"
-nLooseHardLeptons     = "("+mu_loose_hard+"+"+e_loose_hard+")"
-nTightHardLeptons     = "("+mu_tight+"+"+e_tight+")" 
-nLooseSoftPt10Leptons = "("+mu_soft_loose_10Pt+"+"+e_soft_loose_10Pt+")" 
 
 OneMu = "("+mu_tight+"==1"+")"
-OneMu_lepveto = "("+mu_loose+"==1&&"+e_veto+"==0"+")"
+OneMu_lepveto = "("+mu_veto+"==1&&"+e_veto+"==0"+")"
 OneE = "("+e_tight+"==1"+")"
-OneE_lepveto ="("+mu_veto+"==0&&"+e_loose+"==1"+")"
-#OneE_lepveto ="("+mu_tight+"==0&&"+e_loose+"==1"+")"
+OneE_lepveto ="("+mu_veto+"==0&&"+e_veto+"==1"+")"
 OneLep ="("+mu_tight+"+"+e_tight+"==1"+")"
-#OneLep_lepveto = "(("+mu_loose+"+"+e_loose+")==1"+")"
 OneLep_lepveto =  "(("+"abs(LepGood_pdgId)==11&&"+OneE_lepveto+")"+"||"+"("+"abs(LepGood_pdgId)==13&&"+OneMu_lepveto+"))"
 
-#OneMu_lepveto = "("+nLooseHardLeptons+"==1"+"&&"+nTightHardLeptons+"==1"+"&&"+nLooseSoftPt10Leptons+"==0"+")"
-#OneE_lepveto = "("+nLooseHardLeptons+"==1"+"&&"+nTightHardLeptons+"==1"+"&&"+nLooseSoftPt10Leptons+"==0"+")"
-#OneLep_lepveto = "("+nLooseHardLeptons+"==1"+"&&"+nTightHardLeptons+"==1"+"&&"+nLooseSoftPt10Leptons+"==0"+")"
+####njet and ht definitions###
+#njets_30 = "Sum$(Jet_pt>"+str(minPt_Jet)+"&&abs(Jet_eta)<"+str(maxEta_Jet)+"&&Jet_id)"
+njets_30 = "Sum$(Jet_pt>"+str(minPt_Jet)+"&&abs(Jet_eta)<"+str(maxEta_Jet)+")"
+#nbjets_30 = "Sum$(Jet_pt>"+str(minPt_Jet)+"&&abs(Jet_eta)<"+str(maxEta_Jet)+"&&Jet_id&&Jet_btagCSV>"+str(btag_var)+")"
+nbjets_30 = "Sum$(Jet_pt>"+str(minPt_Jet)+"&&abs(Jet_eta)<"+str(maxEta_Jet)+"&&Jet_btagCSV>"+str(btag_var)+")"
 
-njets_30 = "Sum$(Jet_pt>"+str(minPt_Jet)+"&&abs(Jet_eta)<"+str(maxEta_Jet)+"&&Jet_id)"
-nbjets_30 = "Sum$(Jet_pt>"+str(minPt_Jet)+"&&abs(Jet_eta)<"+str(maxEta_Jet)+"&&Jet_id&&Jet_btagCSV>"+str(btag_var)+")"
-
-#st = "((LepGood_pt[0]+met_pt)>"+str(min_st)+")"
-ht = "(Sum$(Jet_pt*(Jet_pt>"+str(minPt_Jet)+"&&abs(Jet_eta)<"+str(maxEta_Jet)+"&&Jet_id)))"
-dPhi = "acos((LepGood_pt[0]+met_pt[0]*cos(LepGood_phi[0]-met_phi[0]))/sqrt((LepGood_pt[0]**2)+(met_pt[0]**2)+(2*met_pt[0]*LepGood_pt[0]*cos(LepGood_phi[0]-met_phi[0]))))"
-#dPhi = "acos((LepGood_pt+met_pt*cos(LepGood_phi-met_phi))/sqrt((LepGood_pt**2)+(met_pt**2)+(2*met_pt*LepGood_pt*cos(LepGood_phi-met_phi))))"
+#ht = "(Sum$(Jet_pt*(Jet_pt>"+str(minPt_Jet)+"&&abs(Jet_eta)<"+str(maxEta_Jet)+"&&Jet_id)))"
+ht = "(Sum$(Jet_pt*(Jet_pt>"+str(minPt_Jet)+"&&abs(Jet_eta)<"+str(maxEta_Jet)+")))"
+dPhi = "acos((LepGood_pt[0]+metNoHF_pt*cos(LepGood_phi[0]-metNoHF_phi))/sqrt((LepGood_pt[0]**2)+(metNoHF_pt**2)+(2*metNoHF_pt*LepGood_pt[0]*cos(LepGood_phi[0]-metNoHF_phi))))"
+#dPhi = "acos((LepGood_pt+metNoHF_pt*cos(LepGood_phi-metNoHF_phi))/sqrt((LepGood_pt**2)+(metNoHF_pt**2)+(2*metNoHF_pt*LepGood_pt*cos(LepGood_phi-metNoHF_phi))))"
 
 
-#def stCut(stb)
-#  if stb[1] == -1: return "(Sum$((LepGood_pt+met_pt)>"+str(stb[0])+")==1)"
-#  else: return "(Sum$((LepGood_pt+met_pt)>"+str(stb[0])+"(LepGood_pt+met_pt)<="+str(stb[1])")==1)"
+#st = "(Sum$(((LepGood_pt[0]+metNoHF_pt)>"+str(min_st)+")*"+OneLep+")==1)"
+st = "(Sum$(((LepGood_pt[0]+metNoHF_pt)>"+str(min_st)+"))==1)"
 
-st = "(Sum$((LepGood_pt+met_pt)>"+str(min_st)+")==1)"
+####njet and ht cuts
 njets_30_cut = "(("+njets_30+")>="+str(min_njets)+")"
-nbjets_30_cut = "(("+nbjets_30+")=="+str(nbjet)+")"
+nbjets_30_cut_zero = "(("+nbjets_30+")=="+str(nbjet)+")"
+nbjets_30_cut_multi = "(("+nbjets_30+")>=1)"
 jets_2_80 = "(Jet_pt[1]>80)"
+#jets_2_80 = "(Jet_pt[1]>80&&abs(Jet_eta)<"+str(maxEta_Jet)+"&&Jet_id))"
 ht_cut = "("+ht+">"+str(min_ht)+")"
 #dPhi_cut = "("+"Sum$("+dPhi+">"+str(min_DPhi)+")==1)"
 dPhi_cut = "("+dPhi+">"+str(min_DPhi)+")"
 
-
+filters = "(Flag_goodVertices&&Flag_CSCTightHaloFilter&&Flag_eeBadScFilter)"
 
 
 
--- RA4Analysis/python/cmgTuples_Spring15_v2.py
+++ RA4Analysis/python/cmgTuples_Spring15_v2.py
@@ -4,6 +4,7 @@ import copy, os, sys
 #artur_path = "/afs/cern.ch/work/e/easilar/Aug06_GoldenJSON/"
 artur_path = "/afs/cern.ch/work/e/easilar/data/data_from_Artur_newMET_13Aug/Aug12_GoldenJson_METnoHF/"
 
+navid_path = "/data/nrad/cmgTuples/RunII/Spring15_v1/"
 
 data_ele={\
 "name" : "SingleElectron_Run2015B",
@@ -45,67 +46,85 @@ data_mu_17July={\
 'isData':True,
 'dir' : artur_path+"/"
 }
-
 ################
 
 TTJets={\
-#"name" : "TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",
-"chunkString" : "RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_plzworkheplx",
-"name" : "RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_plzworkheplx",
-'dir' : "/afs/hephy.at/work/e/easilar/MC_Spring15_Samples/hadded_samples/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/", 
+"chunkString" : "TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test",
+"name" : "TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test",
+'dir' : navid_path+"/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test/", 
 "rootFileLocation":"tree.root",
 "treeName":"tree",
 'isData':False
 }
 
-DY={\
-#"name" : "DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8",
-"chunkString" : "RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2_plzworkheplx",
-"name" : "RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2_plzworkheplx",
-#'dir' : "/tmp/easilar/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2_plzworkheplx/", 
-'dir' : "/afs/hephy.at/work/e/easilar/MC_Spring15_Samples/hadded_samples/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8//", 
+#DY={\
+##"name" : "DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8",
+#"chunkString" : "RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2_plzworkheplx",
+#"name" : "RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2_plzworkheplx",
+##'dir' : "/tmp/easilar/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2_plzworkheplx/", 
+#'dir' : "/afs/hephy.at/work/e/easilar/MC_Spring15_Samples/hadded_samples/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8//", 
+#"rootFileLocation":"tree.root",
+#"treeName":"tree",
+#'isData':False
+#}
+
+DY_HT200to400={\
+"chunkString" : "DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test",
+"name" : "DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test",
+'dir' : navid_path+"/DYJetsToLL_M-50_HT-200to400_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test/", 
+"rootFileLocation":"tree.root",
+"treeName":"tree",
+'isData':False
+}
+
+DY_HT400to600={\
+"chunkString" : "DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test",
+"name" : "DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test",
+'dir' : navid_path+"/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test/", 
+"rootFileLocation":"tree.root",
+"treeName":"tree",
+'isData':False
+}
+DY_HT600toInf={\
+"chunkString" : "DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test",
+"name" : "DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test",
+'dir' : navid_path+"/DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test/", 
 "rootFileLocation":"tree.root",
 "treeName":"tree",
 'isData':False
 }
 
 T_tWch={\
-#"name" : "ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1",
-"chunkString" : "RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_plzworkheplx",
-"name" : "RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_plzworkheplx",
-'dir' : "/afs/hephy.at/work/e/easilar/MC_Spring15_Samples/hadded_samples/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/", 
+"chunkString" : "ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test",
+"name" : "ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test",
+'dir' : navid_path+"/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test/", 
 "rootFileLocation":"tree.root",
 "treeName":"tree",
 'isData':False
 }
 
 TBar_tWch={\
-#"name" : "ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/",
-"chunkString" : "RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2_plzworkheplx",
-"name" : "RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2_plzworkheplx",
-'dir' : "/afs/hephy.at/work/e/easilar/MC_Spring15_Samples/hadded_samples/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/", 
+"chunkString" : "ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2_test",
+"name" : "ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2_test",
+'dir' : navid_path+"/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2_test/", 
 "rootFileLocation":"tree.root",
 "treeName":"tree",
 'isData':False
 }
 
 TToLeptons_tch={\
-#"name" : "ST_t-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1",
-"chunkString" : "RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_plzworkheplx",
-"name" : "RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_plzworkheplx",
-'dir' : "/afs/hephy.at/work/e/easilar/MC_Spring15_Samples/hadded_samples/ST_t-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1/", 
+"chunkString" : "ST_t-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test",
+"name" : "ST_t-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test",
+'dir' : navid_path+"/ST_t-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test/", 
 "rootFileLocation":"tree.root",
 "treeName":"tree",
 'isData':False
 }
 
 WJets={\
-#"name" : "WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8",
-"chunkString" : "RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_plzworkheplx",
-"name" : "RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_plzworkheplx",
-#'dir' : "/tmp/easilar/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_plzworkheplx/",
-#'dir' : "/afs/hephy.at/work/e/easilar/MC_Spring15_Samples/hadded_samples/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/",
-'dir' : "/afs/hephy.at/work/e/easilar/MC_Spring15_Samples/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_plzworkheplx/",
+"chunkString" : "WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test",
+"name" : "WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test",
+'dir' : navid_path+"/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1_test/", 
 "rootFileLocation":"tree.root",
 "treeName":"tree",
 'isData':False
@@ -119,7 +138,7 @@ WJets={\
 #"treeName":"tree",
 #'isData':False
 #}
-
+'''
 QCD_HT200to300={\
 #"name" : "QCD_HT200to300_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/",
 "chunkString" : "RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2_plzworkheplx",
@@ -191,5 +210,5 @@ QCD_HT2000toInf={\
 }
 
 #print QCD_HT500to700
-
+'''
 
