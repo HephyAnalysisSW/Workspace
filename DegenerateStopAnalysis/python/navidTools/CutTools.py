385e5d2b2022bce5cd37983d553fc64500d970d6
Navid Rad <navid.rad@cern.ch>
2015-10-29 13:53:38 +0100

    Update FOMS with the regularized AMS

--- DegenerateStopAnalysis/python/navidTools/CutTools.py
+++ DegenerateStopAnalysis/python/navidTools/CutTools.py
@@ -25,6 +25,7 @@ def joinCutStrings(cutStringList):
 
 
 class CutClass():
+  """ CutClass(Name, cutList = [["cut1name","cut1string"] ,..., ["cut2name","cut2string"]] , baseCut=baseCutClass   ) """
   def __init__(self,name,cutList,baseCut=None):
     self.name         = name
     self.inclList     = cutList
@@ -33,54 +34,55 @@ class CutClass():
     self.inclCombined = self._combine(self.inclList) 
     self.inclCombinedList  = (self.name ,self._combine(self.inclList) )
     self.baseCut = baseCut
-
     if baseCut:
       if isinstance(baseCut,CutClass):
         self.baseCutString      = baseCut.combined
         self.baseCutName        = baseCut.name
+        self.fullList           = self.baseCut.list + self.inclList
       else:
         self.baseCutName, self.baseCutString = baseCut
     else: 
       self.baseCutName, self.baseCutString = (None,None)
-
-
-
     if not self.baseCutString or self.baseCutString == "(1)":
       self.list         = cutList
     else:
       self.list         =[[self.baseCutName, self.baseCutString]]+  [ [cutName,"(%s)"%"&&".join([self.baseCutString,cut])  ] for cutName,cut in self.inclList ]
-
-
-    #self.list         = [ [name,"(%s)"%"&&".join([baseCutString,cut])  ] for name,cut in self.inclList ]
-    #self.dict         = self._makeDict(self.list)
     self.flow         = self._makeFlow(self.list)
     self.combined     = self._combine(self.inclList,self.baseCutString)
     self.combinedList = (self.name, self.combined)
-   # if baseCut and baseCut != "(1)":
-   #   self.baseCut = baseCut
-   #   self.list = [ [name,baseCut+"&&"+cut] for name,cut in cutList ] 
-   #   self.flow = self._makeFlow(self.list)
-   #   self.incl_list = cutList
-   #   self.incl_flow = self._makeFlow(cutList)
-   # else:
-   #   self.list = cutList
-   # self.keys = [x[0] for x in self.list]
-   # self.dict= self._makeDict(self.InclusiveCutList)
-   # self._combine(cutList)
+
   def _makeDict(self,cutList):
     Dict={}
     for cutName, cutString in cutList:
       Dict[cutName]=cutString
     return Dict
+
   def _makeFlow(self,cutList):
     flow=makeCutFlowList(cutList)
     flowDict= self._makeDict(flow)
     return flow
+
   def _combine(self,cutList,baseCutString=None) :
     if not baseCutString or baseCutString == "(1)":
       return combineCutList(cutList)
     else:
       return "(%s &&"%baseCutString+ combineCutList(cutList)+ ")"
+
+  def nMinus1(self,minusCutList, cutList=True, baseCut=False) :
+    if baseCut:
+      cutList = self.fullList
+    else:
+      cutList = self.inclList
+    if not baseCut and cutList:
+      cutList = cutList
+
+    minusCutList = [minusCut.lower() for minusCut in minusCutList]
+    print minusCutList
+    print cutList
+    cutList = filter( lambda x: not x[0].lower() in  minusCutList , cutList)
+    print cutList
+    return combineCutList(cutList)
+
     #if baseCut:
     #  self.combined=combineCutList(self.inclusiveCutList)
     #else: 
--- DegenerateStopAnalysis/python/navidTools/FOM.py
+++ DegenerateStopAnalysis/python/navidTools/FOM.py
@@ -7,19 +7,29 @@ import math
 
 def AMSSYS (s,b,sysUnc=0.2):
   #print s, b
+  sysUnc2=sysUnc*sysUnc
+  b2 = b*b 
   if s==0: 
     return 0
   if b==0: 
     return -1
-  #return (lambda s,b : math.sqrt(2*( (s+b)*math.log(((s+b)*(b+sysUnc*b))/(b*b+(s+b)*sysUnc*b))  - b*b/(sysUnc*b)*math.log(1+sysUnc*b*s/(b*(b+sysUnc*b))) )) if b!=0 else -1)(s,b)
-  ret = math.sqrt(2*( (s+b)*math.log(((s+b)*(b+ (sysUnc*sysUnc*b*b) ))/(b*b+(s+b)* (sysUnc*sysUnc*b*b) ))  - b*b/( (sysUnc*sysUnc*b*b) )*math.log(1+ (sysUnc*sysUnc*b*b) *s/(b*(b+ (sysUnc*sysUnc*b*b) ))))) 
+  #return (lambda s,b : math.sqrt(2*( (s+b)*math.log(((s+b)*(b+sysUnc*b))/(b2+(s+b)*sysUnc*b))  - b2/(sysUnc*b)*math.log(1+sysUnc*b*s/(b*(b+sysUnc*b))) )) if b!=0 else -1)(s,b)
+  ret = math.sqrt(2*( (s+b)*math.log(((s+b)*(b+ (sysUnc2*b2) ))/(b2+(s+b)* (sysUnc2*b2) ))  - b2/( (sysUnc2*b2) )*math.log(1+ (sysUnc2*b2) *s/(b*(b+ (sysUnc2*b2) ))))) 
   return ret 
+
+
+def AMS1 (s,b,sysUnc=0.2):
+  sysUnc2= sysUnc*sysUnc
+  b0 = 0.5*( b - sysUnc2 + math.sqrt( (b-sysUnc2)*(b-sysUnc2)+4*(s+b)*(sysUnc2)   )  ) 
+  ams1 = math.sqrt(2*((s+b)*math.log((s+b)/b0)-s-b+b0 )+ (b-b0)*(b-b0)/sysUnc2  )
+  return ams1
 
 fomFuncs= {
                 "SOB"         : lambda s,b,sysUnc  : s/math.sqrt(b) if b!=0 else -1 ,
                 "SOBSYS"      : lambda s,b,sysUnc : s/math.sqrt(b+(sysUnc*sysUnc*b*b) ) if b!=0 else -1 ,
                 "AMS"         : lambda s,b,sysUnc : math.sqrt(2*((s+b)*math.log(1+1.*s/b)-s) ) if b!=0 else -1 ,
                 "AMSSYS"      : AMSSYS ,
+                "AMS1"        : AMS1   ,
             }
 
 def calcFOMs(s,b,sysUnc=0.2,fom=None):
