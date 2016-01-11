2e881beadaabfaac372c43a96b2d372124f31a8d
Navid Rad <navid.rad@cern.ch>
2016-01-11 14:14:34 +0100

    DegStop Samples Update

--- DegenerateStopAnalysis/python/cmgTuples_Spring15_7412pass2.py
+++ DegenerateStopAnalysis/python/cmgTuples_Spring15_7412pass2.py
@@ -4,7 +4,7 @@ from CMGTools.RootTools.samples.samples_13TeV_RunIISpring15MiniAODv2 import *
 import PhysicsTools.HeppyCore.framework.config as cfg
 from Workspace.HEPHYPythonTools.xsec import xsec
 
-data_path = "/data/nrad/cmgTuples/RunII/7412pass2/RunIISpring15MiniAODv2"
+data_path = "/data/nrad/cmgTuples/RunII/7412pass2_v4/RunIISpring15MiniAODv2/"
 
 
 TTJets_LO = {\
@@ -163,6 +163,141 @@ WJetsToLNu_HT2500toInf ={\
 }
 
 
+
+
+
+ZJetsToNuNu_HT100to200 ={\
+"cmgComp":ZJetsToNuNu_HT100to200,
+"name" :"ZJetsToNuNu_HT-100To200_13TeV-madgraph_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1", 
+"chunkString":"ZJetsToNuNu_HT-100To200_13TeV-madgraph_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"dir": data_path,
+"dbsName" : ZJetsToNuNu_HT100to200.dataset, 
+"rootFileLocation":"tree.root",
+"treeName":"tree",
+'isData':False
+}
+
+ZJetsToNuNu_HT200to400 ={\
+"cmgComp":ZJetsToNuNu_HT200to400,
+"name" :"ZJetsToNuNu_HT-200To400_13TeV-madgraph_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1", 
+"chunkString":"ZJetsToNuNu_HT-200To400_13TeV-madgraph_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"dir": data_path,
+"dbsName" : ZJetsToNuNu_HT200to400.dataset, 
+"rootFileLocation":"tree.root",
+"treeName":"tree",
+'isData':False
+}
+
+
+ZJetsToNuNu_HT400to600 ={\
+"cmgComp":ZJetsToNuNu_HT400to600,
+"name" :"ZJetsToNuNu_HT-400To600_13TeV-madgraph_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1", 
+"chunkString":"ZJetsToNuNu_HT-400To600_13TeV-madgraph_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"dir": data_path,
+"dbsName" : ZJetsToNuNu_HT400to600.dataset, 
+"rootFileLocation":"tree.root",
+"treeName":"tree",
+'isData':False
+}
+
+
+
+ZJetsToNuNu_HT600toInf ={\
+"cmgComp":ZJetsToNuNu_HT600toInf,
+"name" :"ZJetsToNuNu_HT-600ToInf_13TeV-madgraph_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v2", 
+"chunkString":"ZJetsToNuNu_HT-600ToInf_13TeV-madgraph_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v2",
+"dir": data_path,
+"dbsName" : ZJetsToNuNu_HT600toInf.dataset, 
+"rootFileLocation":"tree.root",
+"treeName":"tree",
+'isData':False
+}
+
+
+QCD_HT200to300 = {
+"cmgComp": QCD_HT200to300   ,
+"dbsName" : QCD_HT200to300.dataset,
+"name" : "QCD_HT200to300_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"chunkString" : "QCD_HT200to300_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"dir": data_path,
+"rootFileLocation":"tree.root",
+"treeName":"tree",
+'isData':False
+}
+
+QCD_HT300to500   ={
+"cmgComp": QCD_HT300to500   ,
+"dbsName" : QCD_HT300to500.dataset,
+"name" : "QCD_HT300to500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"chunkString" : "QCD_HT300to500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"dir": data_path,
+"rootFileLocation":"tree.root",
+"treeName":"tree",
+'isData':False
+}
+
+QCD_HT500to700   ={
+"cmgComp":   QCD_HT500to700 ,
+"dbsName" : QCD_HT500to700.dataset,
+"name" : "QCD_HT500to700_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"chunkString" : "QCD_HT500to700_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"dir": data_path,
+"rootFileLocation":"tree.root",
+"treeName":"tree",
+'isData':False
+}
+
+
+QCD_HT700to1000   ={
+"cmgComp":  QCD_HT700to1000  ,
+"dbsName" : QCD_HT700to1000.dataset,
+"name" : "QCD_HT700to1000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"chunkString" : "QCD_HT700to1000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"dir": data_path,
+"rootFileLocation":"tree.root",
+"treeName":"tree",
+'isData':False
+}
+
+QCD_HT1000to1500   ={
+"cmgComp": QCD_HT1000to1500   ,
+"dbsName" : QCD_HT1000to1500.dataset,
+"name" : "QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"chunkString" : "QCD_HT1000to1500_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"rootFileLocation":"tree.root",
+"dir": data_path,
+"treeName":"tree",
+'isData':False
+}
+
+QCD_HT1500to2000   ={
+"cmgComp":  QCD_HT1500to2000  ,
+"dbsName" : QCD_HT1500to2000.dataset,
+"name" : "QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"chunkString" : "QCD_HT1500to2000_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"dir": data_path,
+"rootFileLocation":"tree.root",
+"treeName":"tree",
+'isData':False
+}
+
+QCD_HT2000toInf   ={
+"cmgComp":  QCD_HT2000toInf  ,
+"dbsName" : QCD_HT2000toInf.dataset,
+"name" : "QCD_HT2000toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"chunkString" : "QCD_HT2000toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1",
+"dir": data_path,
+"rootFileLocation":"tree.root",
+"treeName":"tree",
+'isData':False
+}
+
+
+
+
+
+
+
 TTJets = [TTJets_LO, TTJets_LO_HT600to800, TTJets_LO_HT800to1200, TTJets_LO_HT1200to2500, TTJets_LO_HT2500toInf]
 WJetsInc = [WJetsToLNu]
 WJetsHT  = [
@@ -176,7 +311,26 @@ WJetsHT  = [
     WJetsToLNu_HT2500toInf,
     ]
 
-samples = TTJets + WJetsInc + WJetsHT
+ZJetsHT  = [
+    ZJetsToNuNu_HT100to200,
+    ZJetsToNuNu_HT200to400,
+    ZJetsToNuNu_HT400to600,
+    ZJetsToNuNu_HT600toInf
+    ]
+
+QCDHT = [
+
+    QCD_HT200to300,
+    QCD_HT300to500,
+    QCD_HT500to700,
+    QCD_HT700to1000,
+    QCD_HT1000to1500,
+    QCD_HT1500to2000,
+    QCD_HT2000toInf,
+
+    ]
+
+samples = TTJets + WJetsInc + WJetsHT +ZJetsHT + QCDHT
 
 for sample in samples:
 #  print sample
@@ -213,9 +367,10 @@ def getSignalSample(dir, signal):
       'dir' : dir,
       'dbsName':signal,
       'isData':False,
-      "rootFileLocation":"treeProducerSusySingleLepton/tree.root",
+      #"rootFileLocation":"treeProducerSusySingleLepton/tree.root",
+      "rootFileLocation":"tree.root",
       "treeName":"tree",
-      "skimAnalyzerDir":"skimAnalyzerCount",
+      #"skimAnalyzerDir":"skimAnalyzerCount",
       }
   else:
     print "Signal",signal,"unknown. Available: ",", ".join(allSignalStrings)
@@ -228,5 +383,5 @@ for d,s in allSignalData:
 for sample in allSignals:
     sample['xsec'] = xsec[sample['dbsName']]
     sample['cmgComp'].xSection = xsec[sample['dbsName']]
-
+    
 
