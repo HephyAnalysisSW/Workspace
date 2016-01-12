dc7589e77f41dbc6f2bc1c81ad34c21308034bd8
Navid Rad <navid.rad@cern.ch>
2016-01-12 17:54:53 +0100

    DegStop: script for easy PostProcessing

--- DegenerateStopAnalysis/cmgPostProcessing/runPP.py
+++ DegenerateStopAnalysis/cmgPostProcessing/runPP.py
@@ -3,7 +3,7 @@ import subprocess
 
 
 
-TTJets = [
+TTJetsHT = [
      "TTJets_LO",
      "TTJets_LO_HT600to800",
      "TTJets_LO_HT800to1200",
@@ -47,54 +47,79 @@ QCDHT = [
 
 Signals=[
     "T2DegStop_300_270",
-    "T2DegStop_300_290FS",
-    "T2DegStop_300_240FS",
-    "T2DegStop_300_270FS",
+    "T2DegStop_300_290_FastSim",
+    "T2DegStop_300_240_FastSim",
+    "T2DegStop_300_270_FastSim",
 ]
 
 
+allData = [
+    "MET_Run2015D_05Oct", 
+    "MET_Run2015D_v4", 
+    "SingleElectron_Run2015D_05Oct", 
+    "SingleElectron_Run2015D_v4", 
+    "SingleMuon_Run2015D_05Oct", 
+    "SingleMuon_Run2015D_v4",
+        ]
+
+
 #sample=ZJetsHT
-sampleToUse=WJets
+allMC = Signals + WJets + TTJetsHT + ZJetsHT + QCDHT
+sampleToUse = WJets + Signals + allData
 
-nPJobs=10
+
 tag = "7412pass2_v4_012016_v0"
-cmgTuple='Data_25ns'
 
-
-small= True
-overwrite=True
-lepSel="inc"
-processTracks=True
-log="INFO"
-
-
-opts=[]
-if overwrite: opts.append("--overwriteOutputFiles")
-if lepSel: opts.append("--leptonSelection=%s"%lepSel)
-if small: opts.append("--runSmallSample")
-if log: opts.append("--logLevel=%s"%log)
-if cmgTuple: opts.append("--cmgTuples=%s"%cmgTuple )
-opts.append("--processingTag=%s"%tag)
-if processTracks: opts.append("--processTracks")
-#if sample: opts.append("--processSamples=%s"%sample )
-
-
-print opts
+opts = {
+    "tag":tag               ,
+    "overwrite": True       ,
+    "lepSel": 'inc'         ,
+    "small": False          ,
+    "log": 'INFO'           ,
+    "processTracks": False  ,
+    }
 
 
 
+def makePostProcCommand(sample, cmgTuple, tag, overwrite="False", lepSel='inc', small='False', log='INFO',processTracks='False'): 
+    opts=[]
+    opts.append("--processSamples=%s"%sample)
+    if overwrite: opts.append("--overwriteOutputFiles")
+    if lepSel: opts.append("--leptonSelection=%s"%lepSel)
+    if small: opts.append("--runSmallSample")
+    if log: opts.append("--logLevel=%s"%log)
+    if cmgTuple: opts.append("--cmgTuples=%s"%cmgTuple )
+    opts.append("--processingTag=%s"%tag)
+    if processTracks: opts.append("--processTracks")
+    #if sample: opts.append("--processSamples=%s"%sample )
+
+    command=[]
+    command.extend(["python", "cmgPostProcessing_v1.py"]+opts)
+    print ' '.join(command)
+    return ' '.join(command)
+    #return subprocess.call(command)
 
 
 
-command=[]
-test=True
-if test:
-    command.append("echo")
 
-command.extend(["python", "cmgPostProcessing_v1.py"]+opts)
+for sample in allMC:
+    cmgTuple='RunIISpring15DR74_25ns'
+    makePostProcCommand( sample, cmgTuple, **opts)
 
-for sample in sampleToUse:
-    sampleCommand = command + ["--processSamples=%s"%sample, '&']    
-    subprocess.call(sampleCommand)
+for sample in allData:
+    cmgTuple='Data_25ns'
+    makePostProcCommand( sample, cmgTuple, **opts)
+    
+
+
+
+
+
+
+
+
+#for sample in sampleToUse:
+#    sampleCommand = command + ["--processSamples=%s"%sample, '&']    
+#    subprocess.call(sampleCommand)
 
 
