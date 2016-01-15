31999268ff9fe078381f40aeb032080b56d19640
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2016-01-15 17:31:40 +0100

    minor2

--- RA4Analysis/cmgPostProcessing/all_postProcessing_Spring15_25ns_2.sh
+++ RA4Analysis/cmgPostProcessing/all_postProcessing_Spring15_25ns_2.sh
@@ -3,7 +3,7 @@
 
 #python cmgPostProcessing.py --leptonSelection=hard --skim=""  --samples=DYJetsToLL_M_10to50_25ns
 #python cmgPostProcessing.py --skim="HT500ST250"  --samples=DYJetsToLL_M_50_25ns
-python cmgPostProcessing.py --overwrite --skim="HT500ST250"  --samples=DYJetsToLL_M_50_HT100to200_25ns
-python cmgPostProcessing.py --overwrite --skim="HT500ST250"  --samples=DYJetsToLL_M_50_HT200to400_25ns
-python cmgPostProcessing.py --overwrite --skim="HT500ST250"  --samples=DYJetsToLL_M_50_HT400to600_25ns
-python cmgPostProcessing.py --overwrite --skim="HT500ST250"  --samples=DYJetsToLL_M_50_HT600toInf_25ns
+python cmgPostProcessing.py --overwrite  --skim="HT500ST250"  --samples=DYJetsToLL_M_50_HT100to200_25ns
+python cmgPostProcessing.py --overwrite  --skim="HT500ST250"  --samples=DYJetsToLL_M_50_HT200to400_25ns
+python cmgPostProcessing.py --overwrite  --skim="HT500ST250"  --samples=DYJetsToLL_M_50_HT400to600_25ns
+python cmgPostProcessing.py --overwrite  --skim="HT500ST250"  --samples=DYJetsToLL_M_50_HT600toInf_25ns
