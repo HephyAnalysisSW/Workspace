622ec1d4493afbcad3728f6f2286aa447180628b
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2016-01-17 09:29:14 +0100

    use b-tag weights for rare EWK in post processing

--- RA4Analysis/cmgPostProcessing/all_postProcessing_Spring15_25ns_4.sh
+++ RA4Analysis/cmgPostProcessing/all_postProcessing_Spring15_25ns_4.sh
@@ -1,15 +1,15 @@
 #!/bin/sh 
 ########Spring15###############
 
-python cmgPostProcessing.py --overwrite  --skim="HT500ST250"  --samples=TToLeptons_sch
-python cmgPostProcessing.py --overwrite  --skim="HT500ST250"  --samples=TToLeptons_tch
-python cmgPostProcessing.py --overwrite  --skim="HT500ST250"  --samples=TBar_tWch
-python cmgPostProcessing.py --overwrite  --skim="HT500ST250"  --samples=T_tWch
+python cmgPostProcessing.py --overwrite  --calcbtagweights --skim="HT500ST250"  --samples=TToLeptons_sch
+python cmgPostProcessing.py --overwrite  --calcbtagweights --skim="HT500ST250"  --samples=TToLeptons_tch
+python cmgPostProcessing.py --overwrite  --calcbtagweights --skim="HT500ST250"  --samples=TBar_tWch
+python cmgPostProcessing.py --overwrite  --calcbtagweights --skim="HT500ST250"  --samples=T_tWch
 
-python cmgPostProcessing.py --overwrite  --skim="HT500ST250"  --samples=TTWJetsToLNu_25ns
-python cmgPostProcessing.py --overwrite  --skim="HT500ST250"  --samples=TTWJetsToQQ_25ns
-python cmgPostProcessing.py --overwrite  --skim="HT500ST250"  --samples=TTZToLLNuNu_25ns
-python cmgPostProcessing.py --overwrite  --skim="HT500ST250"  --samples=TTZToQQ_25ns
+python cmgPostProcessing.py --overwrite  --calcbtagweights --skim="HT500ST250"  --samples=TTWJetsToLNu_25ns
+python cmgPostProcessing.py --overwrite  --calcbtagweights --skim="HT500ST250"  --samples=TTWJetsToQQ_25ns
+python cmgPostProcessing.py --overwrite  --calcbtagweights --skim="HT500ST250"  --samples=TTZToLLNuNu_25ns
+python cmgPostProcessing.py --overwrite  --calcbtagweights --skim="HT500ST250"  --samples=TTZToQQ_25ns
 
 
 
