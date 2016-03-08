d29ae40fb1856350aaf4d2cd3468880d9f5f9947
Navid Rad <navid.rad@cern.ch>
2016-03-08 14:43:29 +0100

    sample colors

---
+++ DegenerateStopAnalysis/python/colors.py
@@ -0,0 +1,16 @@
+import ROOT
+colors ={
+              'w':             ROOT.kSpring-5       , 
+              'tt':            ROOT.kAzure-5        , 
+              'z':             ROOT.kOrange + 1     , #ROOT.kOrange + 5         ,#ROOT.kSpring+10       
+              #'qcd':           ROOT.kViolet         , 
+              'qcd':           ROOT.kMagenta + 3, #ROOT.kRed -6         , 
+              'wtau':          ROOT.kSpring-2       ,
+              'wnotau':        ROOT.kSpring+2       ,
+
+              "s30":           ROOT.kRed+1          , 
+              "s60FS":         ROOT.kOrange +7      , 
+              "s30FS":         ROOT.kYellow -3       , 
+              "s10FS":         ROOT.kAzure  +7      , 
+              "t2tt30FS":      ROOT.kOrange-1       , 
+            }
