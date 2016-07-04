fa44e0456322a1948bea6221d7c3650b61a6575e
Ece Asilar <ece.asilar@cern.ch>
2016-07-04 20:21:23 +0200

    local

--- RA4Analysis/cmgPostProcessing/all_postProcessing_Spring15_25ns_2.sh
+++ RA4Analysis/cmgPostProcessing/all_postProcessing_Spring15_25ns_2.sh
@@ -6,7 +6,7 @@
 # --calcbtagweights removed!
 #python cmgPostProcessing.py --overwrite --skim="HT500ST250" --calcbtagweights --samples=DYJetsToLL_M_50_amcatnloFXFX_25ns
 #python cmgPostProcessing.py --overwrite --skim="HT500ST250" --calcbtagweights --samples=DYJetsToLL_M_50_madgraphMLM_25ns
-#python cmgPostProcessing.py --overwrite --skim="HT500ST250" --calcbtagweights --samples=DYJetsToLL_M_50_HT_100to200_25ns
-#python cmgPostProcessing.py --overwrite --skim="HT500ST250" --calcbtagweights --samples=DYJetsToLL_M_50_HT_200to400_25ns
-#python cmgPostProcessing.py --overwrite --skim="HT500ST250" --calcbtagweights --samples=DYJetsToLL_M_50_HT_400to600_25ns
+python cmgPostProcessing.py --overwrite --skim="HT500ST250" --calcbtagweights --samples=DYJetsToLL_M_50_HT_100to200_25ns
+python cmgPostProcessing.py --overwrite --skim="HT500ST250" --calcbtagweights --samples=DYJetsToLL_M_50_HT_200to400_25ns
+python cmgPostProcessing.py --overwrite --skim="HT500ST250" --calcbtagweights --samples=DYJetsToLL_M_50_HT_400to600_25ns
 python cmgPostProcessing.py --overwrite --skim="HT500ST250" --calcbtagweights --samples=DYJetsToLL_M_50_HT_600toInf_25ns
--- RA4Analysis/cmgPostProcessing/all_postProcessing_Spring15_25ns_3.sh
+++ RA4Analysis/cmgPostProcessing/all_postProcessing_Spring15_25ns_3.sh
@@ -3,11 +3,11 @@
 
 #python cmgPostProcessing.py --leptonSelection=hard --skim=""  --samples=WJetsToLNu_HT100to200_25ns
 #python cmgPostProcessing.py --overwrite --skim="HT500ST250" --samples=WJetsToLNu_HT100to200_25ns
-python cmgPostProcessing.py --overwrite --skim="HT500ST250"  --samples=WJetsToLNu_HT200to400
-python cmgPostProcessing.py --overwrite --skim="HT500ST250"  --samples=WJetsToLNu_HT400to600
-python cmgPostProcessing.py --overwrite --skim="HT500ST250"  --samples=WJetsToLNu_HT600to800
-python cmgPostProcessing.py --overwrite --skim="HT500ST250"  --samples=WJetsToLNu_HT800to1200
-python cmgPostProcessing.py --overwrite --skim="HT500ST250"  --samples=WJetsToLNu_HT1200to2500
-python cmgPostProcessing.py --overwrite --skim="HT500ST250"  --samples=WJetsToLNu_HT2500toInf
+python cmgPostProcessing.py --overwrite --skim="HT500ST250" --calcbtagweights --samples=WJetsToLNu_HT200to400
+python cmgPostProcessing.py --overwrite --skim="HT500ST250" --calcbtagweights --samples=WJetsToLNu_HT400to600
+python cmgPostProcessing.py --overwrite --skim="HT500ST250" --calcbtagweights --samples=WJetsToLNu_HT600to800
+python cmgPostProcessing.py --overwrite --skim="HT500ST250" --calcbtagweights --samples=WJetsToLNu_HT800to1200
+python cmgPostProcessing.py --overwrite --skim="HT500ST250" --calcbtagweights --samples=WJetsToLNu_HT1200to2500
+python cmgPostProcessing.py --overwrite --skim="HT500ST250" --calcbtagweights --samples=WJetsToLNu_HT2500toInf
 
 #python cmgPostProcessing.py --overwrite --skim="HT500ST250" --calcbtagweights --samples=WJetsToLNu
