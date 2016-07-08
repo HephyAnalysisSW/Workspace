9974b1b19c9376bbe7ef567561e3b949a35bc66a
Mateusz Zarucki <mateusz.zarucki@cern.ch>
2016-07-08 19:37:52 +0200

    Removed unnecessary files

--- DegenerateStopAnalysis/plotsMateusz/genFilter/8TeV/HT/genHTonly/redMassScanHTonly_400.sh
+++
@@ -1,8 +0,0 @@
-DIR=//dpm/oeaw.ac.at/home/cms/store/user/schoef/pat_240614/T2DegenerateStop_2J_mStop-400-4/
-MASSES="400"
-
-python redMassScanHTonly.py --job=57 --first=1 --maxFiles=60 --masses=$MASSES -b $DIR &
-python redMassScanHTonly.py --job=58 --first=60 --maxFiles=60 --masses=$MASSES -b $DIR &
-python redMassScanHTonly.py --job=59 --first=120 --maxFiles=60 --masses=$MASSES -b $DIR &
-python redMassScanHTonly.py --job=60 --first=180 --maxFiles=60 --masses=$MASSES -b $DIR &
-#python redMassScanHTonly.py --job=61 --first=240 --maxFiles=26 --masses=$MASSES -b $DIR &
