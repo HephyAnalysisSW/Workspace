92fa51602637bc8532195ce530e1707bb4573101
Navid Rad <navid.rad@cern.ch>
2016-11-15 13:34:41 +0100

    new data python file

---
+++ DegenerateStopAnalysis/python/samples/cmgTuples/Data2016_v0.py
@@ -0,0 +1,335 @@
+sample_path_tag  = '8020_mAODv2_v0/Data25ns'   
+sample_path_base = '/afs/hephy.at/data/nrad02/cmgTuples/'
+
+allComponents=[] 
+
+import os
+if not os.path.isdir(sample_path_base):
+    sample_path_base = '/data/nrad/cmgTuples/'
+sample_path = sample_path_base + sample_path_tag
+
+
+
+
+MET_Run2016B_PromptReco_v2 ={
+'cmgName':"MET_Run2016B_PromptReco_v2",
+"name" : "MET_Run2016B-PromptReco-v2",
+"chunkString":"MET_Run2016B-PromptReco-v2",
+"dir": sample_path +"/" + "MET_Run2016B-PromptReco-v2",
+"dbsName" : "/MET/Run2016B-PromptReco-v2/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(MET_Run2016B_PromptReco_v2)
+
+
+
+MET_Run2016C_PromptReco_v2 ={
+'cmgName':"MET_Run2016C_PromptReco_v2",
+"name" : "MET_Run2016C-PromptReco-v2",
+"chunkString":"MET_Run2016C-PromptReco-v2",
+"dir": sample_path +"/" + "MET_Run2016C-PromptReco-v2",
+"dbsName" : "/MET/Run2016C-PromptReco-v2/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(MET_Run2016C_PromptReco_v2)
+
+
+
+MET_Run2016D_PromptReco_v2 ={
+'cmgName':"MET_Run2016D_PromptReco_v2",
+"name" : "MET_Run2016D-PromptReco-v2",
+"chunkString":"MET_Run2016D-PromptReco-v2",
+"dir": sample_path +"/" + "MET_Run2016D-PromptReco-v2",
+"dbsName" : "/MET/Run2016D-PromptReco-v2/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(MET_Run2016D_PromptReco_v2)
+
+
+
+MET_Run2016E_PromptReco_v2 ={
+'cmgName':"MET_Run2016E_PromptReco_v2",
+"name" : "MET_Run2016E-PromptReco-v2",
+"chunkString":"MET_Run2016E-PromptReco-v2",
+"dir": sample_path +"/" + "MET_Run2016E-PromptReco-v2",
+"dbsName" : "/MET/Run2016E-PromptReco-v2/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(MET_Run2016E_PromptReco_v2)
+
+
+
+MET_Run2016F_PromptReco_v1 ={
+'cmgName':"MET_Run2016F_PromptReco_v1",
+"name" : "MET_Run2016F-PromptReco-v1",
+"chunkString":"MET_Run2016F-PromptReco-v1",
+"dir": sample_path +"/" + "MET_Run2016F-PromptReco-v1",
+"dbsName" : "/MET/Run2016F-PromptReco-v1/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(MET_Run2016F_PromptReco_v1)
+
+
+
+MET_Run2016G_PromptReco_v1 ={
+'cmgName':"MET_Run2016G_PromptReco_v1",
+"name" : "MET_Run2016G-PromptReco-v1",
+"chunkString":"MET_Run2016G-PromptReco-v1",
+"dir": sample_path +"/" + "MET_Run2016G-PromptReco-v1",
+"dbsName" : "/MET/Run2016G-PromptReco-v1/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(MET_Run2016G_PromptReco_v1)
+
+
+
+SingleElectron_Run2016B_PromptReco_v2 ={
+'cmgName':"SingleElectron_Run2016B_PromptReco_v2",
+"name" : "SingleElectron_Run2016B-PromptReco-v2",
+"chunkString":"SingleElectron_Run2016B-PromptReco-v2",
+"dir": sample_path +"/" + "SingleElectron_Run2016B-PromptReco-v2",
+"dbsName" : "/SingleElectron/Run2016B-PromptReco-v2/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(SingleElectron_Run2016B_PromptReco_v2)
+
+
+
+SingleElectron_Run2016C_PromptReco_v2 ={
+'cmgName':"SingleElectron_Run2016C_PromptReco_v2",
+"name" : "SingleElectron_Run2016C-PromptReco-v2",
+"chunkString":"SingleElectron_Run2016C-PromptReco-v2",
+"dir": sample_path +"/" + "SingleElectron_Run2016C-PromptReco-v2",
+"dbsName" : "/SingleElectron/Run2016C-PromptReco-v2/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(SingleElectron_Run2016C_PromptReco_v2)
+
+
+
+SingleElectron_Run2016D_PromptReco_v2 ={
+'cmgName':"SingleElectron_Run2016D_PromptReco_v2",
+"name" : "SingleElectron_Run2016D-PromptReco-v2",
+"chunkString":"SingleElectron_Run2016D-PromptReco-v2",
+"dir": sample_path +"/" + "SingleElectron_Run2016D-PromptReco-v2",
+"dbsName" : "/SingleElectron/Run2016D-PromptReco-v2/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(SingleElectron_Run2016D_PromptReco_v2)
+
+
+
+SingleElectron_Run2016E_PromptReco_v2 ={
+'cmgName':"SingleElectron_Run2016E_PromptReco_v2",
+"name" : "SingleElectron_Run2016E-PromptReco-v2",
+"chunkString":"SingleElectron_Run2016E-PromptReco-v2",
+"dir": sample_path +"/" + "SingleElectron_Run2016E-PromptReco-v2",
+"dbsName" : "/SingleElectron/Run2016E-PromptReco-v2/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(SingleElectron_Run2016E_PromptReco_v2)
+
+
+
+SingleElectron_Run2016F_PromptReco_v1 ={
+'cmgName':"SingleElectron_Run2016F_PromptReco_v1",
+"name" : "SingleElectron_Run2016F-PromptReco-v1",
+"chunkString":"SingleElectron_Run2016F-PromptReco-v1",
+"dir": sample_path +"/" + "SingleElectron_Run2016F-PromptReco-v1",
+"dbsName" : "/SingleElectron/Run2016F-PromptReco-v1/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(SingleElectron_Run2016F_PromptReco_v1)
+
+
+
+SingleElectron_Run2016G_PromptReco_v1 ={
+'cmgName':"SingleElectron_Run2016G_PromptReco_v1",
+"name" : "SingleElectron_Run2016G-PromptReco-v1",
+"chunkString":"SingleElectron_Run2016G-PromptReco-v1",
+"dir": sample_path +"/" + "SingleElectron_Run2016G-PromptReco-v1",
+"dbsName" : "/SingleElectron/Run2016G-PromptReco-v1/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(SingleElectron_Run2016G_PromptReco_v1)
+
+
+
+SingleMuon_Run2016B_PromptReco_v2 ={
+'cmgName':"SingleMuon_Run2016B_PromptReco_v2",
+"name" : "SingleMuon_Run2016B-PromptReco-v2",
+"chunkString":"SingleMuon_Run2016B-PromptReco-v2",
+"dir": sample_path +"/" + "SingleMuon_Run2016B-PromptReco-v2",
+"dbsName" : "/SingleMuon/Run2016B-PromptReco-v2/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(SingleMuon_Run2016B_PromptReco_v2)
+
+
+
+SingleMuon_Run2016C_PromptReco_v2 ={
+'cmgName':"SingleMuon_Run2016C_PromptReco_v2",
+"name" : "SingleMuon_Run2016C-PromptReco-v2",
+"chunkString":"SingleMuon_Run2016C-PromptReco-v2",
+"dir": sample_path +"/" + "SingleMuon_Run2016C-PromptReco-v2",
+"dbsName" : "/SingleMuon/Run2016C-PromptReco-v2/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(SingleMuon_Run2016C_PromptReco_v2)
+
+
+
+SingleMuon_Run2016D_PromptReco_v2 ={
+'cmgName':"SingleMuon_Run2016D_PromptReco_v2",
+"name" : "SingleMuon_Run2016D-PromptReco-v2",
+"chunkString":"SingleMuon_Run2016D-PromptReco-v2",
+"dir": sample_path +"/" + "SingleMuon_Run2016D-PromptReco-v2",
+"dbsName" : "/SingleMuon/Run2016D-PromptReco-v2/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(SingleMuon_Run2016D_PromptReco_v2)
+
+
+
+SingleMuon_Run2016E_PromptReco_v2 ={
+'cmgName':"SingleMuon_Run2016E_PromptReco_v2",
+"name" : "SingleMuon_Run2016E-PromptReco-v2",
+"chunkString":"SingleMuon_Run2016E-PromptReco-v2",
+"dir": sample_path +"/" + "SingleMuon_Run2016E-PromptReco-v2",
+"dbsName" : "/SingleMuon/Run2016E-PromptReco-v2/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(SingleMuon_Run2016E_PromptReco_v2)
+
+
+
+SingleMuon_Run2016F_PromptReco_v1 ={
+'cmgName':"SingleMuon_Run2016F_PromptReco_v1",
+"name" : "SingleMuon_Run2016F-PromptReco-v1",
+"chunkString":"SingleMuon_Run2016F-PromptReco-v1",
+"dir": sample_path +"/" + "SingleMuon_Run2016F-PromptReco-v1",
+"dbsName" : "/SingleMuon/Run2016F-PromptReco-v1/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(SingleMuon_Run2016F_PromptReco_v1)
+
+
+
+SingleMuon_Run2016G_PromptReco_v1 ={
+'cmgName':"SingleMuon_Run2016G_PromptReco_v1",
+"name" : "SingleMuon_Run2016G-PromptReco-v1",
+"chunkString":"SingleMuon_Run2016G-PromptReco-v1",
+"dir": sample_path +"/" + "SingleMuon_Run2016G-PromptReco-v1",
+"dbsName" : "/SingleMuon/Run2016G-PromptReco-v1/MINIAOD",
+"rootFileLocation":"tree.root",
+"skimAnalyzerDir":"skimAnalyzerCount",
+"treeName":"tree",
+"isData": True,
+"xsec": None,
+
+
+}
+allComponents.append(SingleMuon_Run2016G_PromptReco_v1)
+
