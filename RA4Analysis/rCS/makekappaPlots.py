cd2699309a8cdf664e41820e82f577bdf9ae5379
Ece Asilar <ece.asilar@cern.ch>
2016-12-21 23:41:22 +0100

    isr norm

--- RA4Analysis/rCS/makekappaPlots.py
+++ RA4Analysis/rCS/makekappaPlots.py
@@ -111,7 +111,7 @@ for srNJet in sorted(sideBand3fb):
                          {'chain':cDiboson,   'cut':cut1bCRtt, 'weight':weight_str+'*weightBTag1p_SF'},\
                           ]
 
-      ttJets_samples_0b =[{'chain':cTTJets,     'cut':cut1bCRtt,'weight':weight_str+'*weightBTag0_SF*'+top_ISR_weight}]
+      ttJets_samples_0b =[{'chain':cTTJets,     'cut':cut1bCRtt,'weight':weight_str+'*weightBTag0_SF*'+top_ISR_weight+"*1.071"}]
       #rcs1bCRewk = getRCS(cEWK, cut1bCRtt, dPhiCut, weight_str+"*"+"weightBTag1_SF")
       rcs1bCRewk = combineRCS(ewk_samples_1b, dPhiCut)
       #rcs2bCRewk = getRCS(cEWK, cut2bCRtt, dPhiCut, weight_str+"*"+"weightBTag1p_SF")
